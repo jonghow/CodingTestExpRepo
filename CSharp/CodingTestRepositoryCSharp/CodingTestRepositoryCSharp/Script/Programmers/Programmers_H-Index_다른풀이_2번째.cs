ee612501815d2025082da2993daffba9fe12b015
jonghow <sjh15348@gmail.com>
2024-08-21 05:29:24 +0900

    ProblemSolve_Programmers_H-Index
    Programmers_H-Index

---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/Programmers/Programmers_H-Index_다른풀이_2번째.cs
@@ -0,0 +1,41 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Web;
+//using System.Xml.Linq;
+
+//public class Solution
+//{
+//    public int solution(int[] citations)
+//    {
+//        int answer = 0;
+//        Array.Sort(citations);
+//        List<int> list = new List<int>();
+
+//        if (citations.Length == 1)
+//            return citations[0];
+
+//        for (int i = 0; i < citations.Length; ++i)
+//            list.Add(citations[i]);
+
+//        list.Sort((int a, int b) => { return b.CompareTo(a); });
+
+//        for (int i = 0; i < list.Count; ++i)
+//        {
+//            int index = i + 1;
+//            int count = list.FindAll(rhs => rhs >= index).Count;
+
+//            if (index <= count)
+//                answer = index;
+//        }
+
+
+//        return answer;
+//    }
+//}
+
+
+
