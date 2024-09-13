89c8c4c02a96c0a1eaeaaf26d2836a321737838f
jonghow <sin15348@naver.com>
2024-09-14 02:49:28 +0900

    ProblemFail_BOJ_10972_다음순열
    https://www.acmicpc.net/submit/10972

---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_10972_다음순열_순열_FAIL.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+/*
+ * URL : https://www.acmicpc.net/submit/10972
+ */
+
+namespace CodingTestProj
+{
+    internal class Program
+    {
+        public static string input;
+        public static int _standardIndex; // (n) < (n-1) 가 되는 기준
+
+        static void Main(string[] args)
+        {
+            _standardIndex = -1;
+            int cnt = Int32.Parse(Console.ReadLine());
+            input = Console.ReadLine().Trim().Replace(" ", "");
+
+            for (int i = input.Length - 1; i > 0; --i)
+            {
+                int n = Int32.Parse(input[i].ToString());
+                int prevn = Int32.Parse(input[i - 1].ToString());
+
+                if (n > prevn)
+                {
+                    _standardIndex = i - 1;
+                    break;
+                }
+            }
+            // 분리되는 기준점 찾기
+
+            if (_standardIndex == -1)
+            {
+                Console.Write("-1");
+                return;
+            }
+            // 못 찾았다면, -1 출력 및 종료
+
+            string leftStr = input.Substring(0, _standardIndex + 1);
+            string rightStr = input.Substring(_standardIndex + 1);
+
+            // 찾았다면 왼쪽 오른쪽 영역 분리
+
+            bool _isChange = false;
+
+            for (int i = rightStr.Length - 1; i >= 0; --i)
+            {
+                for (int j = leftStr.Length - 1; j >= 0; --j)
+                {
+                    if (Convert.ToInt32(rightStr[i]) > Convert.ToInt32(leftStr[j]))
+                    {
+                        char[] rightArr = rightStr.ToCharArray();
+                        char[] leftArr = leftStr.ToCharArray();
+
+                        char temp = rightArr[i];
+                        rightArr[i] = leftArr[j];
+                        leftArr[j] = temp;
+
+                        rightStr = new string(rightArr);
+                        leftStr = new string(leftArr);
+                        _isChange = true;
+                        break;
+                    }
+                }
+
+                if (_isChange == true)
+                    break;
+            }
+            // 오른쪽 영역의 오른쪽 원소부터 왼쪽 영역의 오른쪽 원소부터 차례로 큰 부분을 찾는 로직
+
+            char[] rightArr2 = rightStr.ToCharArray();
+            Array.Sort(rightArr2);
+            rightStr = new string(rightArr2);
+            // 오른쪽 영역을 오름차순 정렬
+
+            for (int i = 0; i < leftStr.Length; ++i)
+            {
+                Console.Write($"{Int32.Parse(leftStr[i].ToString())}");
+                Console.Write($" ");
+            }
+
+            for (int i = 0; i < rightStr.Length; ++i)
+            {
+                Console.Write($"{Int32.Parse(rightStr[i].ToString())}");
+
+                if (i != rightStr.Length - 1)
+                    Console.Write($" ");
+            }
+
+            // 왼쪽 먼저 출력하고, 오른쪽 영역 차례로 출력.
+        }
+    }
+}
+
+
+
+
---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_10973_이전순열_순열_FAIL.cs
@@ -0,0 +1,128 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+
+///*
+// * URL : https://www.acmicpc.net/submit/10973
+// */
+
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        public static int N;
+//        static void Main(string[] args)
+//        {
+//            Solution._set();
+//            N = Int32.Parse(Console.ReadLine());
+//            string[] inputs = Console.ReadLine().Split(' ');
+//            string inputStr = string.Empty;
+
+//            for (int i = 0; i < inputs.Length; ++i)
+//            {
+//                inputStr += inputs[i];
+//            }
+//            // 입력 종료
+
+//            // 마지막에서 2번째니까 내림차순하고, 2번만
+//            char[] chr = inputStr.ToCharArray();
+
+//            if (Solution.isFirstCheck(chr))
+//            {
+//                Console.WriteLine($"-1");
+//                return;
+//            }
+
+//            Array.Sort(chr);
+//            Array.Reverse(chr);
+//            int[] arr = new int[chr.Length];
+
+//            for (int i = 0; i < arr.Length; ++i)
+//                arr[i] = Int32.Parse(chr[i].ToString());
+
+//            Solution.next_permutation(arr, N, 4, 0);
+//            Solution.print();
+//        }
+//    }
+
+//    public static class Solution
+//    {
+//        public static List<int>[] _permutaionList;
+//        public static int _permuteCnt;
+
+//        public static void _set()
+//        {
+//            _permuteCnt = 0;
+//            _permutaionList = new List<int>[2];
+//            // 2개만
+//        }
+
+//        public static void next_permutation(int[] arr, int current, int r, int depth)
+//        {
+//            if (r == depth)
+//            {
+//                if (_permuteCnt > 1)
+//                    return;
+
+//                if (_permutaionList[_permuteCnt] == null)
+//                    _permutaionList[_permuteCnt] = new List<int>();
+
+//                for (int i = 0; i < arr.Length; ++i)
+//                    _permutaionList[_permuteCnt].Add(arr[i]);
+
+//                ++_permuteCnt;
+//                return;
+//            }
+
+//            for (int i = depth; i < current; ++i)
+//            {
+//                swap(ref arr[depth], ref arr[i]);
+//                next_permutation(arr, current, r, depth + 1);
+//                swap(ref arr[i], ref arr[depth]);
+//            }
+//        }
+
+//        public static void swap(ref int a, ref int b)
+//        {
+//            int temp = a;
+//            a = b;
+//            b = temp;
+//        }
+
+//        public static void print()
+//        {
+//            for (int i = 0; i < _permutaionList[1].Count; ++i)
+//            {
+//                Console.Write(_permutaionList[1][i]);
+//                if (i != _permutaionList[1].Count - 1)
+//                    Console.Write(' ');
+//            }
+//        }
+
+//        public static bool isFirstCheck(char[] chr)
+//        {
+//            // 제공한 숫자와 같은지 체크
+//            char[] origin = new char[chr.Length];
+//            chr.CopyTo(origin, 0);
+
+//            Array.Sort(chr);
+//            bool ret = true;
+
+//            for (int i = 0; i < chr.Length; ++i)
+//            {
+//                if (origin[i] != chr[i])
+//                {
+//                    ret = false;
+//                    break;
+//                }
+//            }
+//            return ret;
+//        }
+//    }
+//}
+
+
+
+
