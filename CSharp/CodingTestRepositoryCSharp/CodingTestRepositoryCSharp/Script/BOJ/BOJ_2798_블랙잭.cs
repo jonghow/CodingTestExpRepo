4bb33db68781db5d3d9416c24e1e71745b52a04b
jonghow <sjh15348@gmail.com>
2024-10-01 23:30:43 +0900

    Update BOJ_2798_블랙잭.cs
    조금 더 Greedy 하게 풀이

--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_2798_블랙잭.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_2798_블랙잭.cs
@@ -50,23 +50,23 @@ namespace CodingTestProj
             ret = 0;
             balance = int.MaxValue;
             var input = Console.ReadLine().Split(' ');
-            n = Int32.Parse(input[0]);  
+            n = Int32.Parse(input[0]);
             m = Int32.Parse(input[1]);
 
             List<int> list = new List<int>();
 
             input = Console.ReadLine().Split(' ');
 
-            for(int i = 0; i < input.Length; ++i)
+            for (int i = 0; i < input.Length; ++i)
             {
                 list.Add(Int32.Parse(input[i]));
             }
 
-            for(int i = 0; i < list.Count; ++i)
+            for (int i = 0; i < list.Count; ++i)
             {
-                for(int j =  i + 1; j < list.Count; ++j)
+                for (int j = i + 1; j < list.Count; ++j)
                 {
-                    for(int k = j + 1; k < list.Count; ++k)
+                    for (int k = j + 1; k < list.Count; ++k)
                     {
 
                         var value1 = list[i];
@@ -79,10 +79,19 @@ namespace CodingTestProj
 
                         var checkRet = Math.Abs(sum - m);
 
-                        if(checkRet < balance)
+
+
+                        if (checkRet < balance)
                         {
                             balance = checkRet;
                             ret = sum;
+                        }
+
+                        if (checkRet == 0)
+                        {
+                            ret = sum;
+                            Console.WriteLine(ret);
+                            return;
                         }
                     }
                 }
