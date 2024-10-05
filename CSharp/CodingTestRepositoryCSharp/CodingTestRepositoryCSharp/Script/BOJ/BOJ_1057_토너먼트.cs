4bfc42bbf7629dca380c7dcba004b881f59cfded
jonghow <sjh15348@gmail.com>
2024-10-05 21:59:24 +0900

    ProblemSolve_BOJ_1057_토너먼트
    https://www.acmicpc.net/problem/1057

---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1057_토너먼트.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Authentication;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml.Linq;
+using static CodingTestProj.Program;
+
+/*
+ * Difficulty : Easy
+ * URL : https://www.acmicpc.net/problem/1057
+ * Time : 59m 17s
+ */
+
+
+namespace CodingTestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.solve();
+        }
+    }
+    public class Solution
+    {
+        int people;
+        decimal n;
+        decimal m;
+
+        decimal nRound;
+
+        public void solve()
+        {
+            nRound = 1;
+            string[] input = Console.ReadLine().Split(' ');
+
+            people = Int32.Parse(input[0]);
+            n = decimal.Parse(input[1]);
+            m = decimal.Parse(input[2]);
+
+            if(m % 2 == 0 && m -1 == n)
+            {
+                Console.WriteLine(1);
+                return;
+            }
+
+            finder(n,m, nRound);
+
+            Console.WriteLine(nRound);
+        }
+
+        public void finder(decimal var1 ,decimal var2, decimal round)
+        {
+            if (nRound > ((people / 2) + 10))
+                return;
+
+            if (var1 % 2 == 1 && var2 == var1 +1)
+            {
+                nRound = round;
+                return;
+            }
+
+            if (var2 % 2 == 1 && var1 == var2 + 1)
+            {
+                nRound = round;
+                return;
+            }
+
+            var var1next = (var1 % 2 == 1) && (var1 != 1) ? var1 + 1 : var1;
+            var var2next = (var2 % 2 == 1) && (var2 != 1) ? var2 + 1 : var2;
+
+            var var1Div = (var1 == 1) ? 1 : var1next / 2;
+            var var2Div = (var2 == 1) ? 1 : var2next / 2;
+
+            finder(var1Div, var2Div, ++round);
+        }
+    }
+}
+
--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_19532_수학은비대면강의입니다_FAIL.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_19532_수학은비대면강의입니다_FAIL.cs
@@ -1,73 +1,73 @@
-﻿using System;
-using System.CodeDom;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.Design;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Security.AccessControl;
-using System.Security.Authentication;
-using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Web;
-using System.Xml.Linq;
-using static CodingTestProj.Program;
+﻿//using System;
+//using System.CodeDom;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.ComponentModel.Design;
+//using System.Diagnostics;
+//using System.IO;
+//using System.Linq;
+//using System.Runtime.CompilerServices;
+//using System.Runtime.InteropServices;
+//using System.Security.AccessControl;
+//using System.Security.Authentication;
+//using System.Security.Cryptography;
+//using System.Security.Cryptography.X509Certificates;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using System.Web;
+//using System.Xml.Linq;
+//using static CodingTestProj.Program;
 
-/*
- * Difficulty : Easy
- * URL : https://www.acmicpc.net/problem/19532
- * Time : 1h Over
- */
+///*
+// * Difficulty : Easy
+// * URL : https://www.acmicpc.net/problem/19532
+// * Time : 1h Over
+// */
 
 
-namespace CodingTestProj
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Solution solution = new Solution();
-            solution.solve();
-        }
-    }
-    public class Solution
-    {
-        public decimal a;
-        public decimal b;
-        public decimal c;
-        public decimal d;
-        public decimal e;
-        public decimal f;
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Solution solution = new Solution();
+//            solution.solve();
+//        }
+//    }
+//    public class Solution
+//    {
+//        public decimal a;
+//        public decimal b;
+//        public decimal c;
+//        public decimal d;
+//        public decimal e;
+//        public decimal f;
 
-        /*
-         * ax+by = c
-         * dx+ey = f
-         */
-        public void solve()
-        {
-            string[] input = Console.ReadLine().Split(' '); 
+//        /*
+//         * ax+by = c
+//         * dx+ey = f
+//         */
+//        public void solve()
+//        {
+//            string[] input = Console.ReadLine().Split(' '); 
 
-            a = decimal.Parse(input[0]) == 0 ? 1 : decimal.Parse(input[0]);
-            b = decimal.Parse(input[1]) == 0 ? 1 : decimal.Parse(input[1]);
-            c = decimal.Parse(input[2]) == 0 ? 1 : decimal.Parse(input[2]);
-            d = decimal.Parse(input[3]) == 0 ? 1 : decimal.Parse(input[3]);
-            e = decimal.Parse(input[4]) == 0 ? 1 : decimal.Parse(input[4]);
-            f = decimal.Parse(input[5]) == 0 ? 1 : decimal.Parse(input[5]);
+//            a = decimal.Parse(input[0]) == 0 ? 1 : decimal.Parse(input[0]);
+//            b = decimal.Parse(input[1]) == 0 ? 1 : decimal.Parse(input[1]);
+//            c = decimal.Parse(input[2]) == 0 ? 1 : decimal.Parse(input[2]);
+//            d = decimal.Parse(input[3]) == 0 ? 1 : decimal.Parse(input[3]);
+//            e = decimal.Parse(input[4]) == 0 ? 1 : decimal.Parse(input[4]);
+//            f = decimal.Parse(input[5]) == 0 ? 1 : decimal.Parse(input[5]);
 
-            // x 기준으로 맞추자.
-            decimal y = (decimal)((((c*d) /a)- f) / ((b * d / a) - e));
-            decimal x = (decimal)(((((c * e) / (b)) - f)) / (((a * e) / b) - d));
+//            // x 기준으로 맞추자.
+//            decimal y = (decimal)((((c*d) /a)- f) / ((b * d / a) - e));
+//            decimal x = (decimal)(((((c * e) / (b)) - f)) / (((a * e) / b) - d));
 
-            if (x < -999 || x > 999 || y < -999 || y > 999) return;
+//            if (x < -999 || x > 999 || y < -999 || y > 999) return;
 
-            Console.Write($"{(int)x} {(int)y}");
-        }
-    }
-}
+//            Console.Write($"{(int)x} {(int)y}");
+//        }
+//    }
+//}
 
