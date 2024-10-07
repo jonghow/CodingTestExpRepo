f71fc224866d1761a5a8c6d212ef1e71eccd70d3
jonghow <sjh15348@gmail.com>
2024-10-07 23:56:14 +0900

    ProblemSolve_BOJ_3040_백설_공주와_일곱_난쟁이
    https://www.acmicpc.net/problem/3040

---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_3040_백설_공주와_일곱_난쟁이.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Authentication;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml.Linq;
+using static CodingTestProj.Program;
+
+/*
+ * Difficulty : Middle
+ * URL : https://www.acmicpc.net/problem/3040
+ * Time : 
+ */
+
+
+namespace CodingTestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.solve();
+        }
+    }
+    public class Solution
+    {
+        int _count = 9;
+        int _target = 100;
+
+        bool _isPass = false;
+
+        public void solve()
+        {
+            int[] arr = new int[9];
+
+            for (int i = 0; i < _count; ++i)
+            {
+                int val = Int32.Parse(Console.ReadLine());
+                arr[i] = val;
+            }
+            
+            int[] conbination = new int[7];
+            Combination(arr, conbination, 7, 0, 0);
+        }
+
+        public void Combination(int[] arr, int[] combination, int r, int pos, int depth)
+        {
+            if(r == 0)
+            {
+                int val = 0;
+
+                for(int i =0; i < combination.Length; ++i)
+                    val += combination[i];
+
+                if(val == 100)
+                {
+                    for (int i = 0; i < combination.Length; ++i)
+                        Console.WriteLine(combination[i]);
+                }
+            }
+            else if (depth == arr.Length)
+            {
+                return;
+            }
+            else
+            {
+                combination[pos] = arr[depth];
+
+                Combination(arr, combination, r - 1, pos + 1, depth + 1);
+                Combination(arr, combination, r  , pos, depth + 1);
+            }
+        }
+    }
+}
+
