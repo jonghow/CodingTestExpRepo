98f02af8648163274f30eb6920b8a32a4f8a326b
jonghow <sjh15348@gmail.com>
2024-10-17 00:35:16 +0900

    ProblemSolve_BOJ_1065_한수_다른풀이
    https://www.acmicpc.net/problem/1065

--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1065_한수.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1065_한수.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Net.NetworkInformation;
 using static CodingTestProj.Program;
 
 /*
@@ -10,6 +8,70 @@ using static CodingTestProj.Program;
   * Time : 
  */
 
+
+// 0.등차수열 공식을 이용한 풀이법
+
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var ss = new Solution();
+//            ss.Solve();
+//        }
+//    }
+
+//    public class Solution
+//    {
+//        public int n;
+//        public int _ret;
+//        public int[] _arr;
+
+//        public void Solve()
+//        {
+//            n = Int32.Parse(Console.ReadLine());
+
+//            if (n < 100)
+//            {
+//                _ret = n;
+//            }
+//            else
+//            {
+//                _ret += 99; // 99 이상부터 시작
+//                int d = 0;
+//                for (int i = 100; i <= n; ++i)
+//                {
+//                    // 100 ~ n 까지 반복
+//                    string _input = i.ToString();
+//                    int _length = _input.Length;
+
+//                    int a = Convert.ToInt32(_input[0].ToString());
+//                    d = Convert.ToInt32(_input[1].ToString()) - Convert.ToInt32(_input[0].ToString());
+
+//                    for (int j = 1; j < _length; ++j)
+//                    {
+//                        int _val = Convert.ToInt32(_input[j].ToString());
+
+//                        if (_val != (a + (j * d)))
+//                            break;
+
+//                        if (j == (_length - 1))
+//                            ++_ret;
+//                    }
+//                }
+//            }
+//            print();
+//        }
+
+//        public void print()
+//        {
+//            Console.WriteLine(_ret);
+//        }
+//    }
+//}
+
+// 1.배열의 완점탐색을 이용한 풀이법
 
 namespace CodingTestProj
 {
@@ -30,9 +92,9 @@ namespace CodingTestProj
 
         public void Solve()
         {
-             n = Int32.Parse(Console.ReadLine());
+            n = Int32.Parse(Console.ReadLine());
 
-            if(n < 100)
+            if (n < 100)
             {
                 _ret = n;
             }
@@ -40,29 +102,30 @@ namespace CodingTestProj
             {
                 _ret += 99; // 99 이상부터 시작
                 int d = 0;
-                for(int i = 100; i <= n; ++i)
+                for (int i = 100; i <= n; ++i)
                 {
                     // 100 ~ n 까지 반복
                     string _input = i.ToString();
                     int _length = _input.Length;
 
                     int a = Convert.ToInt32(_input[0].ToString());
-                    d = Convert.ToInt32(_input[1].ToString())- Convert.ToInt32(_input[0].ToString());
+                    d = Convert.ToInt32(_input[1].ToString()) - Convert.ToInt32(_input[0].ToString());
 
-                    for (int j = 1; j < _length; ++j)
+                    for (int j = 1; j < _length-1; ++j)
                     {
-                        int _val = Convert.ToInt32(_input[j].ToString());
+                        int _cur = Convert.ToInt32(_input[j].ToString());
+                        int _next = Convert.ToInt32(_input[j+1].ToString());
 
-                        if (_val != (a + (j * d)))
+                        int _new_d = _next - _cur;
+
+                        if (d != _new_d)
                             break;
 
-                        if (j == (_length - 1))
+                        if (j == (_length - 2))
                             ++_ret;
                     }
                 }
             }
-
-
             print();
         }
 
@@ -72,4 +135,3 @@ namespace CodingTestProj
         }
     }
 }
-
