e918862de9ce9e8b118272dc9d6b7bee291028e6
jonghow <sjh15348@gmail.com>
2024-10-17 00:08:54 +0900

    ProblemSolve_BOJ_1065_한수
    https://www.acmicpc.net/problem/1065

---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1065_한수.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.NetworkInformation;
+using static CodingTestProj.Program;
+
+/*
+ * Difficulty : Easy ~ Middle
+ * URL : https://www.acmicpc.net/problem/1065
+  * Time : 
+ */
+
+
+namespace CodingTestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ss = new Solution();
+            ss.Solve();
+        }
+    }
+
+    public class Solution
+    {
+        public int n;
+        public int _ret;
+        public int[] _arr;
+
+        public void Solve()
+        {
+             n = Int32.Parse(Console.ReadLine());
+
+            if(n < 100)
+            {
+                _ret = n;
+            }
+            else
+            {
+                _ret += 99; // 99 이상부터 시작
+                int d = 0;
+                for(int i = 100; i <= n; ++i)
+                {
+                    // 100 ~ n 까지 반복
+                    string _input = i.ToString();
+                    int _length = _input.Length;
+
+                    int a = Convert.ToInt32(_input[0].ToString());
+                    d = Convert.ToInt32(_input[1].ToString())- Convert.ToInt32(_input[0].ToString());
+
+                    for (int j = 1; j < _length; ++j)
+                    {
+                        int _val = Convert.ToInt32(_input[j].ToString());
+
+                        if (_val != (a + (j * d)))
+                            break;
+
+                        if (j == (_length - 1))
+                            ++_ret;
+                    }
+                }
+            }
+
+
+            print();
+        }
+
+        public void print()
+        {
+            Console.WriteLine(_ret);
+        }
+    }
+}
+
--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_2303_숫자_게임.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_2303_숫자_게임.cs
@@ -1,108 +1,108 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Net.NetworkInformation;
-using static CodingTestProj.Program;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Net.NetworkInformation;
+//using static CodingTestProj.Program;
 
-/*
- * Difficulty : 
- * URL : https://www.acmicpc.net/problem/2303
-  * Time : 
- */
-
-
-namespace CodingTestProj
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var ss = new Solution();
-            ss.Solve();
-        }
-    }
-
-    public class Solution
-    {
-        public int n;
-
-        public List<int>[] _list;
-
-        int _lastNumb; // 일의 자리 숫자
-        int _ret;
-
-        public int[][] _arr;
-
-        public void Solve()
-        {
-            _ret = 0;
-            _lastNumb = 0;
-            int n = Int32.Parse(Console.ReadLine());
-            _arr = new int[n][];
-
-            for(int i = 0; i < n; ++i)
-            {
-                _arr[i] = new int[5];
-            }
-
-            for (int i = 0; i < n; i++)
-            {
-                string[] _input = Console.ReadLine().Split(' ');
-
-                for (int j= 0; j < _input.Length; ++j)
-                    _arr[i][j] = int.Parse(_input[j]);
-            }
-
-            for(int i = 0; i < n; ++i)
-            {
-                int[] _c = new int[5];
-                combination(_arr[i], _c, 3, 0, 0, i+1);
-            }
-
-            print();
-        }
-
-        public void combination(int[] _arr, int[] _com, int r, int pos,int depth, int _person)
-        {
-            if (r == 0)
-            {
-                var _val1 = _com[0];
-                var _val2 = _com[1];
-                var _val3 = _com[2];
-
-                var ret = _val1 + _val2 + _val3;
-
-                while (ret >= 10)
-                {
-                    ret %= 10;
-                }
-
-                if (ret >= _lastNumb)
-                {
-                    _lastNumb = ret;
-
-                    //if (_ret <= _person)
-                        _ret = _person;
-                }
-
-                return;
-            }
-            else if (depth == _arr.Length)
-                return;
-            else
-            {
-                _com[pos] = _arr[depth];
-
-                combination(_arr, _com, r - 1, pos + 1, depth + 1, _person);
-                combination(_arr, _com, r, pos, depth + 1, _person);
-            }
-        }
+///*
+// * Difficulty : 
+// * URL : https://www.acmicpc.net/problem/2303
+//  * Time : 
+// */
 
 
-        public void print()
-        {
-            Console.WriteLine(_ret);
-        }
-    }
-}
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var ss = new Solution();
+//            ss.Solve();
+//        }
+//    }
+
+//    public class Solution
+//    {
+//        public int n;
+
+//        public List<int>[] _list;
+
+//        int _lastNumb; // 일의 자리 숫자
+//        int _ret;
+
+//        public int[][] _arr;
+
+//        public void Solve()
+//        {
+//            _ret = 0;
+//            _lastNumb = 0;
+//            int n = Int32.Parse(Console.ReadLine());
+//            _arr = new int[n][];
+
+//            for(int i = 0; i < n; ++i)
+//            {
+//                _arr[i] = new int[5];
+//            }
+
+//            for (int i = 0; i < n; i++)
+//            {
+//                string[] _input = Console.ReadLine().Split(' ');
+
+//                for (int j= 0; j < _input.Length; ++j)
+//                    _arr[i][j] = int.Parse(_input[j]);
+//            }
+
+//            for(int i = 0; i < n; ++i)
+//            {
+//                int[] _c = new int[5];
+//                combination(_arr[i], _c, 3, 0, 0, i+1);
+//            }
+
+//            print();
+//        }
+
+//        public void combination(int[] _arr, int[] _com, int r, int pos,int depth, int _person)
+//        {
+//            if (r == 0)
+//            {
+//                var _val1 = _com[0];
+//                var _val2 = _com[1];
+//                var _val3 = _com[2];
+
+//                var ret = _val1 + _val2 + _val3;
+
+//                while (ret >= 10)
+//                {
+//                    ret %= 10;
+//                }
+
+//                if (ret >= _lastNumb)
+//                {
+//                    _lastNumb = ret;
+
+//                    //if (_ret <= _person)
+//                        _ret = _person;
+//                }
+
+//                return;
+//            }
+//            else if (depth == _arr.Length)
+//                return;
+//            else
+//            {
+//                _com[pos] = _arr[depth];
+
+//                combination(_arr, _com, r - 1, pos + 1, depth + 1, _person);
+//                combination(_arr, _com, r, pos, depth + 1, _person);
+//            }
+//        }
+
+
+//        public void print()
+//        {
+//            Console.WriteLine(_ret);
+//        }
+//    }
+//}
 
