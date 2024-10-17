9c99ea9d5ec731350e3ed57c65f1b0d4d685015c
jonghow <sjh15348@gmail.com>
2024-10-17 23:12:04 +0900

    ProblemSolve_BOJ_1251_단어_나누기
    https://www.acmicpc.net/problem/1251

--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1065_한수.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1065_한수.cs
@@ -1,15 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using static CodingTestProj.Program;
+﻿//using System;
+//using System.Collections.Generic;
+//using static CodingTestProj.Program;
 
-/*
- * Difficulty : Easy ~ Middle
- * URL : https://www.acmicpc.net/problem/1065
-  * Time : 
- */
+///*
+// * Difficulty : Easy ~ Middle
+// * URL : https://www.acmicpc.net/problem/1065
+//  * Time : 
+// */
 
 
-// 0.등차수열 공식을 이용한 풀이법
+//// 0.등차수열 공식을 이용한 풀이법
+
+////namespace CodingTestProj
+////{
+////    internal class Program
+////    {
+////        static void Main(string[] args)
+////        {
+////            var ss = new Solution();
+////            ss.Solve();
+////        }
+////    }
+
+////    public class Solution
+////    {
+////        public int n;
+////        public int _ret;
+////        public int[] _arr;
+
+////        public void Solve()
+////        {
+////            n = Int32.Parse(Console.ReadLine());
+
+////            if (n < 100)
+////            {
+////                _ret = n;
+////            }
+////            else
+////            {
+////                _ret += 99; // 99 이상부터 시작
+////                int d = 0;
+////                for (int i = 100; i <= n; ++i)
+////                {
+////                    // 100 ~ n 까지 반복
+////                    string _input = i.ToString();
+////                    int _length = _input.Length;
+
+////                    int a = Convert.ToInt32(_input[0].ToString());
+////                    d = Convert.ToInt32(_input[1].ToString()) - Convert.ToInt32(_input[0].ToString());
+
+////                    for (int j = 1; j < _length; ++j)
+////                    {
+////                        int _val = Convert.ToInt32(_input[j].ToString());
+
+////                        if (_val != (a + (j * d)))
+////                            break;
+
+////                        if (j == (_length - 1))
+////                            ++_ret;
+////                    }
+////                }
+////            }
+////            print();
+////        }
+
+////        public void print()
+////        {
+////            Console.WriteLine(_ret);
+////        }
+////    }
+////}
+
+//// 1.배열의 완점탐색을 이용한 풀이법
 
 //namespace CodingTestProj
 //{
@@ -49,14 +111,17 @@ using static CodingTestProj.Program;
 //                    int a = Convert.ToInt32(_input[0].ToString());
 //                    d = Convert.ToInt32(_input[1].ToString()) - Convert.ToInt32(_input[0].ToString());
 
-//                    for (int j = 1; j < _length; ++j)
+//                    for (int j = 1; j < _length-1; ++j)
 //                    {
-//                        int _val = Convert.ToInt32(_input[j].ToString());
+//                        int _cur = Convert.ToInt32(_input[j].ToString());
+//                        int _next = Convert.ToInt32(_input[j+1].ToString());
 
-//                        if (_val != (a + (j * d)))
+//                        int _new_d = _next - _cur;
+
+//                        if (d != _new_d)
 //                            break;
 
-//                        if (j == (_length - 1))
+//                        if (j == (_length - 2))
 //                            ++_ret;
 //                    }
 //                }
@@ -70,68 +135,3 @@ using static CodingTestProj.Program;
 //        }
 //    }
 //}
-
-// 1.배열의 완점탐색을 이용한 풀이법
-
-namespace CodingTestProj
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var ss = new Solution();
-            ss.Solve();
-        }
-    }
-
-    public class Solution
-    {
-        public int n;
-        public int _ret;
-        public int[] _arr;
-
-        public void Solve()
-        {
-            n = Int32.Parse(Console.ReadLine());
-
-            if (n < 100)
-            {
-                _ret = n;
-            }
-            else
-            {
-                _ret += 99; // 99 이상부터 시작
-                int d = 0;
-                for (int i = 100; i <= n; ++i)
-                {
-                    // 100 ~ n 까지 반복
-                    string _input = i.ToString();
-                    int _length = _input.Length;
-
-                    int a = Convert.ToInt32(_input[0].ToString());
-                    d = Convert.ToInt32(_input[1].ToString()) - Convert.ToInt32(_input[0].ToString());
-
-                    for (int j = 1; j < _length-1; ++j)
-                    {
-                        int _cur = Convert.ToInt32(_input[j].ToString());
-                        int _next = Convert.ToInt32(_input[j+1].ToString());
-
-                        int _new_d = _next - _cur;
-
-                        if (d != _new_d)
-                            break;
-
-                        if (j == (_length - 2))
-                            ++_ret;
-                    }
-                }
-            }
-            print();
-        }
-
-        public void print()
-        {
-            Console.WriteLine(_ret);
-        }
-    }
-}
---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1251_단어_나누기.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static CodingTestProj.Program;
+
+/*
+ * Difficulty : Middle
+ * URL : https://www.acmicpc.net/problem/1251
+  * Time : 43m
+ */
+
+
+//mobitel
+namespace CodingTestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ss = new Solution();
+            ss.Solve();
+        }
+    }
+
+    public class Solution
+    {
+        public string _in;
+        public string _ret;
+
+        List<string> list = new List<string>();
+        List<string> _sorted_list = new List<string>();
+
+
+        public void Solve()
+        {
+            _ret = string.Empty;
+            _in = Console.ReadLine();
+
+            for(int i = 1; i < _in.Length -1; ++i)
+            {
+                string copy = _in;
+                list.Clear();
+                string _1 = copy.Substring(0, i);
+
+                for (int j= 1; (j+i) < _in.Length; ++j)
+                {
+                    string _2 = copy.Substring(i, j);
+                    string _3 = copy.Substring(j+1+(i-1),copy.Length-(1+j+(i-1)));
+
+                    char[] _1arr= _1.ToCharArray();
+                    char[] _2arr= _2.ToCharArray();
+                    char[] _3arr = _3.ToCharArray();
+
+                    Array.Reverse(_1arr);
+                    Array.Reverse(_2arr);
+                    Array.Reverse(_3arr);
+
+                    string __1 = new string(_1arr);
+                    string __2 = new string(_2arr);
+                    string __3 = new string(_3arr);
+
+                    string _combine = __1 + __2 + __3;
+
+                    _sorted_list.Add(_combine);
+                }
+            }
+
+            _sorted_list.Sort();
+
+            if (_sorted_list.Count > 0)
+                _ret = _sorted_list[0];
+
+            print();
+        }
+
+        public void print()
+        {
+            Console.WriteLine(_ret);
+        }
+    }
+}
