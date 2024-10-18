59c5ef083df957d3af978c00c1c2265d1951d45c
jonghow <sjh15348@gmail.com>
2024-10-18 23:55:00 +0900

    ProblemSolve_BOJ_1059_좋은구간
    https://www.acmicpc.net/problem/1059

--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1251_단어_나누기.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1251_단어_나누기.cs
@@ -1,81 +1,78 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using static CodingTestProj.Program;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using static CodingTestProj.Program;
 
-/*
- * Difficulty : Middle
- * URL : https://www.acmicpc.net/problem/1251
-  * Time : 43m
- */
-
-
-//mobitel
-namespace CodingTestProj
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var ss = new Solution();
-            ss.Solve();
-        }
-    }
-
-    public class Solution
-    {
-        public string _in;
-        public string _ret;
-
-        List<string> list = new List<string>();
-        List<string> _sorted_list = new List<string>();
+///*
+// * Difficulty : Middle
+// * URL : https://www.acmicpc.net/problem/1251
+//  * Time : 43m
+// */
 
 
-        public void Solve()
-        {
-            _ret = string.Empty;
-            _in = Console.ReadLine();
+////mobitel
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var ss = new Solution();
+//            ss.Solve();
+//        }
+//    }
 
-            for(int i = 1; i < _in.Length -1; ++i)
-            {
-                string copy = _in;
-                list.Clear();
-                string _1 = copy.Substring(0, i);
+//    public class Solution
+//    {
+//        public string _in;
+//        public string _ret;
 
-                for (int j= 1; (j+i) < _in.Length; ++j)
-                {
-                    string _2 = copy.Substring(i, j);
-                    string _3 = copy.Substring(j+1+(i-1),copy.Length-(1+j+(i-1)));
+//        List<string> _sorted_list = new List<string>();
 
-                    char[] _1arr= _1.ToCharArray();
-                    char[] _2arr= _2.ToCharArray();
-                    char[] _3arr = _3.ToCharArray();
+//        public void Solve()
+//        {
+//            _ret = string.Empty;
+//            _in = Console.ReadLine();
 
-                    Array.Reverse(_1arr);
-                    Array.Reverse(_2arr);
-                    Array.Reverse(_3arr);
+//            for(int i = 1; i < _in.Length -1; ++i)
+//            {
+//                string copy = _in;
+//                string _1 = copy.Substring(0, i);
 
-                    string __1 = new string(_1arr);
-                    string __2 = new string(_2arr);
-                    string __3 = new string(_3arr);
+//                for (int j= 1; (j+i) < _in.Length; ++j)
+//                {
+//                    string _2 = copy.Substring(i, j);
+//                    string _3 = copy.Substring(j+1+(i-1),copy.Length-(1+j+(i-1)));
 
-                    string _combine = __1 + __2 + __3;
+//                    char[] _1arr= _1.ToCharArray();
+//                    char[] _2arr= _2.ToCharArray();
+//                    char[] _3arr = _3.ToCharArray();
 
-                    _sorted_list.Add(_combine);
-                }
-            }
+//                    Array.Reverse(_1arr);
+//                    Array.Reverse(_2arr);
+//                    Array.Reverse(_3arr);
 
-            _sorted_list.Sort();
+//                    string __1 = new string(_1arr);
+//                    string __2 = new string(_2arr);
+//                    string __3 = new string(_3arr);
 
-            if (_sorted_list.Count > 0)
-                _ret = _sorted_list[0];
+//                    string _combine = __1 + __2 + __3;
 
-            print();
-        }
+//                    _sorted_list.Add(_combine);
+//                }
+//            }
 
-        public void print()
-        {
-            Console.WriteLine(_ret);
-        }
-    }
-}
+//            _sorted_list.Sort();
+
+//            if (_sorted_list.Count > 0)
+//                _ret = _sorted_list[0];
+
+//            print();
+//        }
+
+//        public void print()
+//        {
+//            Console.WriteLine(_ret);
+//        }
+//    }
+//}
