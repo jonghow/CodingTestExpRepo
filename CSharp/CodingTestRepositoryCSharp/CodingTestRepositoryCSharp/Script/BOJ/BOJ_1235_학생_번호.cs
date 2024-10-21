e6714d63cc47d34927605161c201e969148dceed
jonghow <sjh15348@gmail.com>
2024-10-21 23:48:38 +0900

    ProblemSolve_BOJ_1235_학생번호
    ProblemSolve_BOJ_1235_학생번호
    https://www.acmicpc.net/problem/1235
    
    ProblemFail_BOJ_17614_369
    https://www.acmicpc.net/problem/17614

--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1235_학생_번호.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1235_학생_번호.cs
@@ -1,99 +1,100 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using static CodingTestProj.Program;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using static CodingTestProj.Program;
 
-/*
- * Difficulty : Easy ~ Middle
- * URL : https://www.acmicpc.net/problem/1235
-  * Time : 
- */
+///*
+// * Difficulty : Middle
+// * URL : https://www.acmicpc.net/problem/1235
+//  * Time : 1h 3m
+// */
 
 
-//mobitel
-namespace CodingTestProj
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var ss = new Solution();
-            ss.Solve();
-        }
-    }
+////mobitel
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var ss = new Solution();
+//            ss.Solve();
+//        }
+//    }
 
-    public class Solution
-    {
-        public int n;
-        public int ret;
-        public List<string> list_number = new List<string>();
+//    public class Solution
+//    {
+//        public int n;
+//        public int ret;
+//        public List<string> list_number = new List<string>();
 
-        public List<string> list_sorted = new List<string>();
+//        public List<string> list_sorted = new List<string>();
 
-        public char[,] _arr2Axis;
+//        public char[,] _arr2Axis;
 
-        public void Solve()
-        {
-            ret = int.MaxValue ;
-            n = Int32.Parse(Console.ReadLine());
-            int _length = 0;
+//        public void Solve()
+//        {
+//            ret = int.MaxValue ;
+//            n = Int32.Parse(Console.ReadLine());
+//            int _length = 0;
             
-            for(int i = 0; i < n; ++i)
-            {
-                string _in = Console.ReadLine();
-                _length = _in.Length;
+//            for(int i = 0; i < n; ++i)
+//            {
+//                string _in = Console.ReadLine();
+//                _length = _in.Length;
 
-                if (_arr2Axis == null)
-                    _arr2Axis = new char[_in.Length, n];
+//                if (_arr2Axis == null)
+//                    _arr2Axis = new char[_in.Length, n];
 
-                for(int j = 0; j < _in.Length; ++j)
-                {
-                    _arr2Axis[j,i] = _in[j];
-                }
-            }
+//                for(int j = 0; j < _in.Length; ++j)
+//                {
+//                    _arr2Axis[j,i] = _in[j];
+//                }
+//            }
 
-            // 유동적이다 몇번재 번호가 주어지는지는.
-            // 다 9 자리 일 수도있고, 100번째일수도있음.
+//            // 유동적이다 몇번재 번호가 주어지는지는.
+//            // 다 9 자리 일 수도있고, 100번째일수도있음.
 
-            for (int i = _length-1  ; i >= 0; --i)
-            {
-                list_sorted.Clear();
-                for (int j = 0; j < n; ++j)
-                {
-                    string ret = string.Empty;
-                    for(int k = 0; k < (_length-i); ++k)
-                    {
-                        ret += _arr2Axis[i-k,j];
-                    }
+//            for (int i = _length-1  ; i >= 0; --i)
+//            {
+//                list_sorted.Clear();
+//                for (int j = 0; j < n; ++j)
+//                {
+//                    string ret = string.Empty;
+//                    for(int k = 0; k < _length - i; ++k)
+//                    {
+//                        ret += _arr2Axis[i+k,j];
+//                    }
 
-                    list_sorted.Add(ret);
-                }
+//                    list_sorted.Add(ret);
+//                }
 
-                list_sorted.Sort();
+//                list_sorted.Sort();
 
 
-                bool _isPass = true;
-                for (int k = 0; k < list_sorted.Count - 1; ++k)
-                {
-                    if (list_sorted[k] == list_sorted[k + 1])
-                    {
-                        _isPass = false;
-                        break;
-                    }
-                }
+//                bool _isPass = true;
+//                for (int k = 0; k < list_sorted.Count - 1; ++k)
+//                {
+//                    if (list_sorted[k] == list_sorted[k + 1])
+//                    {
+//                        _isPass = false;
+//                        break;
+//                    }
+//                }
 
-                if (_isPass == true)
-                {
-                    ++ret;
-                }
-            }
+//                if (_isPass == true)
+//                {
+//                    ret = _length - i;
+//                    break;
+//                }
+//            }
 
-            print();
-        }
+//            print();
+//        }
 
-        public void print()
-        {
-            Console.WriteLine(ret);
-        }
-    }
-}
+//        public void print()
+//        {
+//            Console.WriteLine(ret);
+//        }
+//    }
+//}
---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_5555_반지.cs
@@ -0,0 +1,58 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using static CodingTestProj.Program;
+
+///*
+// * Difficulty : Easy
+// * URL : https://www.acmicpc.net/problem/5555
+//  * Time : 7m
+// */
+
+
+////mobitel
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var ss = new Solution();
+//            ss.Solve();
+//        }
+//    }
+
+//    public class Solution
+//    {
+//        public int n;
+//        public int ret;
+
+//        public string target;
+
+//        public void Solve()
+//        {
+//            ret = 0;
+//            target = string.Empty;
+
+//            target = Console.ReadLine();
+//            n = Int32.Parse(Console.ReadLine());
+
+//            for(int i  = 0; i < n; ++i)
+//            {
+//                string input = Console.ReadLine();
+//                input += input;
+
+
+//                if (input.Contains(target) == true)
+//                    ++ret;
+//            }
+
+//            print();
+//        }
+
+//        public void print()
+//        {
+//            Console.WriteLine(ret);
+//        }
+//    }
+//}
