40401898f384253553fd00c1b29c6c34265e8478
jonghow <sjh15348@gmail.com>
2024-10-22 23:59:25 +0900

    ProblemSolve_BOJ_17614_369
    https://www.acmicpc.net/problem/17614

--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1018_체스판_다시칠하기.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1018_체스판_다시칠하기.cs
@@ -1,141 +1,141 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using static CodingTestProj.Program;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using static CodingTestProj.Program;
 
-/*
- * Difficulty : Middle
- * URL : https://www.acmicpc.net/problem/1018
-  * Time : 35m
- */
+///*
+// * Difficulty : Middle
+// * URL : https://www.acmicpc.net/problem/1018
+//  * Time : 35m
+// */
 
-namespace CodingTestProj
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var ss = new Solution();
-            ss.Solve();
-        }
-    }
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var ss = new Solution();
+//            ss.Solve();
+//        }
+//    }
 
-    public class Solution
-    {
-        public int x;
-        public int y;
+//    public class Solution
+//    {
+//        public int x;
+//        public int y;
 
-        public int ret;
+//        public int ret;
 
-        public char[,] _arr_v1 = new char[8, 8]
-        {
-            { 'B' ,'W','B','W','B','W','B','W'},
-            { 'W' ,'B','W','B','W','B','W','B'},
-            { 'B' ,'W','B','W','B','W','B','W'},
-            { 'W' ,'B','W','B','W','B','W','B'},
-            { 'B' ,'W','B','W','B','W','B','W'},
-            { 'W' ,'B','W','B','W','B','W','B'},
-            { 'B' ,'W','B','W','B','W','B','W'},
-            { 'W' ,'B','W','B','W','B','W','B'},
-        };
+//        public char[,] _arr_v1 = new char[8, 8]
+//        {
+//            { 'B' ,'W','B','W','B','W','B','W'},
+//            { 'W' ,'B','W','B','W','B','W','B'},
+//            { 'B' ,'W','B','W','B','W','B','W'},
+//            { 'W' ,'B','W','B','W','B','W','B'},
+//            { 'B' ,'W','B','W','B','W','B','W'},
+//            { 'W' ,'B','W','B','W','B','W','B'},
+//            { 'B' ,'W','B','W','B','W','B','W'},
+//            { 'W' ,'B','W','B','W','B','W','B'},
+//        };
 
-        public char[,] _arr_v2 = new char[8, 8]
-        {
-            { 'W' ,'B','W','B','W','B','W','B'},
-            { 'B' ,'W','B','W','B','W','B','W'},
-            { 'W' ,'B','W','B','W','B','W','B'},
-            { 'B' ,'W','B','W','B','W','B','W'},
-            { 'W' ,'B','W','B','W','B','W','B'},
-            { 'B' ,'W','B','W','B','W','B','W'},
-            { 'W' ,'B','W','B','W','B','W','B'},
-            { 'B' ,'W','B','W','B','W','B','W'},
-        };
+//        public char[,] _arr_v2 = new char[8, 8]
+//        {
+//            { 'W' ,'B','W','B','W','B','W','B'},
+//            { 'B' ,'W','B','W','B','W','B','W'},
+//            { 'W' ,'B','W','B','W','B','W','B'},
+//            { 'B' ,'W','B','W','B','W','B','W'},
+//            { 'W' ,'B','W','B','W','B','W','B'},
+//            { 'B' ,'W','B','W','B','W','B','W'},
+//            { 'W' ,'B','W','B','W','B','W','B'},
+//            { 'B' ,'W','B','W','B','W','B','W'},
+//        };
 
-        public char[,] _board;
+//        public char[,] _board;
 
-        public void Solve()
-        {
-            ret = int.MaxValue;
-            string[] input = Console.ReadLine().Split(' ');
+//        public void Solve()
+//        {
+//            ret = int.MaxValue;
+//            string[] input = Console.ReadLine().Split(' ');
 
-            x = Int32.Parse(input[0]);
-            y = Int32.Parse(input[1]);
+//            x = Int32.Parse(input[0]);
+//            y = Int32.Parse(input[1]);
 
-            _board = new char[x, y];
+//            _board = new char[x, y];
 
-            for (int i = 0; i < x; ++i)
-            {
-                string inBoard = Console.ReadLine();
-                for(int j = 0; j < inBoard.Length; ++j)
-                {
-                    _board[i,j] = inBoard[j];
-                }
-            }
+//            for (int i = 0; i < x; ++i)
+//            {
+//                string inBoard = Console.ReadLine();
+//                for(int j = 0; j < inBoard.Length; ++j)
+//                {
+//                    _board[i,j] = inBoard[j];
+//                }
+//            }
 
-            int curPosX = 0;
-            int curPosY = 0;
+//            int curPosX = 0;
+//            int curPosY = 0;
 
-            for(int i = 0; i < y - 7;  ++i)
-            {
-                for(int j = 0; j < x - 7; ++j)
-                {
-                    curPosX = j;
-                    curPosY = i;
+//            for(int i = 0; i < y - 7;  ++i)
+//            {
+//                for(int j = 0; j < x - 7; ++j)
+//                {
+//                    curPosX = j;
+//                    curPosY = i;
 
-                    Find(curPosX, curPosY);
-                }
-            }
+//                    Find(curPosX, curPosY);
+//                }
+//            }
 
-            print();
-        }
+//            print();
+//        }
 
-        public void Find(int posX, int posY)
-        {
-            char[,] useCopyBoard = new char[y, x];
-            useCopyBoard = _arr_v1;
+//        public void Find(int posX, int posY)
+//        {
+//            char[,] useCopyBoard = new char[y, x];
+//            useCopyBoard = _arr_v1;
 
-            int _ret = 0;
+//            int _ret = 0;
 
-            for (int i = 0; i < 8; ++i)
-            {
-                for(int j = 0; j < 8;++j)
-                {
-                    int newPosX = posX + j;
-                    int newPosY = posY + i;
+//            for (int i = 0; i < 8; ++i)
+//            {
+//                for(int j = 0; j < 8;++j)
+//                {
+//                    int newPosX = posX + j;
+//                    int newPosY = posY + i;
 
-                    if (_board[newPosX, newPosY] == useCopyBoard[j, i])
-                        continue;
+//                    if (_board[newPosX, newPosY] == useCopyBoard[j, i])
+//                        continue;
 
-                    ++_ret;
-                }
-            }
+//                    ++_ret;
+//                }
+//            }
 
-            if (ret >= _ret)
-                ret = _ret;
+//            if (ret >= _ret)
+//                ret = _ret;
 
-            useCopyBoard = _arr_v2;
-            _ret = 0;
-            for (int i = 0; i < 8; ++i)
-            {
-                for (int j = 0; j < 8; ++j)
-                {
-                    int newPosX = posX + j;
-                    int newPosY = posY + i;
+//            useCopyBoard = _arr_v2;
+//            _ret = 0;
+//            for (int i = 0; i < 8; ++i)
+//            {
+//                for (int j = 0; j < 8; ++j)
+//                {
+//                    int newPosX = posX + j;
+//                    int newPosY = posY + i;
 
-                    if (_board[newPosX, newPosY] == useCopyBoard[j, i])
-                        continue;
+//                    if (_board[newPosX, newPosY] == useCopyBoard[j, i])
+//                        continue;
 
-                    ++_ret;
-                }
-            }
+//                    ++_ret;
+//                }
+//            }
 
-            if (ret >= _ret)
-                ret = _ret;
-        }
+//            if (ret >= _ret)
+//                ret = _ret;
+//        }
 
-        public void print()
-        {
-            Console.WriteLine(ret);
-        }
-    }
-}
+//        public void print()
+//        {
+//            Console.WriteLine(ret);
+//        }
+//    }
+//}
--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_17614_369.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_17614_369.cs
@@ -1,55 +1,82 @@
-﻿//using System;
-//using System.Collections;
-//using System.Collections.Generic;
-//using static CodingTestProj.Program;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static CodingTestProj.Program;
 
-///*
-// * Difficulty : 
-// * URL : https://www.acmicpc.net/problem/17614
-//  * Time : 
-// */
+/*
+ * Difficulty : 
+ * URL : https://www.acmicpc.net/problem/17614
+  * Time : 
+ */
 
 
-////mobitel
-//namespace CodingTestProj
-//{
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            var ss = new Solution();
-//            ss.Solve();
-//        }
-//    }
+//mobitel
+namespace CodingTestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ss = new Solution();
+            ss.Solve();
+        }
+    }
 
-//    public class Solution
-//    {
-//        public int n;
-//        public int ret;
+    public class Solution
+    {
+        public int n;
+        public int ret;
 
-//        public string target;
+        public string target;
 
-//        public void Solve()
-//        {
-//            ret = 0;
-//            n = Int32.Parse(Console.ReadLine());
+        public void Solve()
+        {
+            ret = 0;
+            n = Int32.Parse(Console.ReadLine());
 
-//            int index = 1;
-//            while(n == index)
-//            {
-//                int val = index;
+            for (int i = 1; i <= n; ++i)
+            {
+                string strN = i.ToString();
 
-//                ++index;
-//            }
+                while(true)
+                {
+                    if (strN.Contains("3") == false &&
+                        strN.Contains("6") == false &&
+                        strN.Contains("9") == false)
+                        break;
 
-            
+                    if(strN.Contains("3") == true)
+                    {
+                        int index = strN.IndexOf("3");
+                        strN = strN.Remove(index, 1);
+                        ++ret;
+                        continue;
+                    }
 
-//            print();
-//        }
+                    if (strN.Contains("6") == true)
+                    {
+                        int index = strN.IndexOf("6");
+                        strN = strN.Remove(index, 1);
+                        ++ret;
+                        continue;
+                    }
 
-//        public void print()
-//        {
-//            Console.WriteLine(ret);
-//        }
-//    }
-//}
+                    if (strN.Contains("9") == true)
+                    {
+                        int index = strN.IndexOf("9");
+                        strN = strN.Remove(index, 1);
+                        ++ret;
+                        continue;
+                    }
+                }
+            }
+
+            print();
+        }
+
+        public void print()
+        {
+            Console.WriteLine(ret);
+        }
+    }
+}
