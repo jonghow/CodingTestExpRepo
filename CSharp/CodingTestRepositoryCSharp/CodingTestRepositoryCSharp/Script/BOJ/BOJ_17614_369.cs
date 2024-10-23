f5f700ffd15cf6eeba989cca2e42c66c4b20e324
jonghow <sjh15348@gmail.com>
2024-10-23 23:58:03 +0900

    ProblemSolve_BOJ_17614_369
    https://www.acmicpc.net/problem/17614

--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_17614_369.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_17614_369.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using static CodingTestProj.Program;
 
-/*
+/*sdsd
  * Difficulty : 
  * URL : https://www.acmicpc.net/problem/17614
   * Time : 
