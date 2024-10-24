348cecfbc92e45b8c49ad780becdce8d66753492
jonghow <sjh15348@gmail.com>
2024-10-24 23:36:41 +0900

    ProblemFail_BOJ_18429_근손실
    https://www.acmicpc.net/problem/18429

--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_17614_369.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_17614_369.cs
@@ -1,82 +1,82 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using static CodingTestProj.Program;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using static CodingTestProj.Program;
 
-/*sdsd
- * Difficulty : 
- * URL : https://www.acmicpc.net/problem/17614
-  * Time : 
- */
+///*sdsd
+// * Difficulty : 
+// * URL : https://www.acmicpc.net/problem/17614
+//  * Time : 
+// */
 
 
-//mobitel
-namespace CodingTestProj
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var ss = new Solution();
-            ss.Solve();
-        }
-    }
+////mobitel
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var ss = new Solution();
+//            ss.Solve();
+//        }
+//    }
 
-    public class Solution
-    {
-        public int n;
-        public int ret;
+//    public class Solution
+//    {
+//        public int n;
+//        public int ret;
 
-        public string target;
+//        public string target;
 
-        public void Solve()
-        {
-            ret = 0;
-            n = Int32.Parse(Console.ReadLine());
+//        public void Solve()
+//        {
+//            ret = 0;
+//            n = Int32.Parse(Console.ReadLine());
 
-            for (int i = 1; i <= n; ++i)
-            {
-                string strN = i.ToString();
+//            for (int i = 1; i <= n; ++i)
+//            {
+//                string strN = i.ToString();
 
-                while(true)
-                {
-                    if (strN.Contains("3") == false &&
-                        strN.Contains("6") == false &&
-                        strN.Contains("9") == false)
-                        break;
+//                while(true)
+//                {
+//                    if (strN.Contains("3") == false &&
+//                        strN.Contains("6") == false &&
+//                        strN.Contains("9") == false)
+//                        break;
 
-                    if(strN.Contains("3") == true)
-                    {
-                        int index = strN.IndexOf("3");
-                        strN = strN.Remove(index, 1);
-                        ++ret;
-                        continue;
-                    }
+//                    if(strN.Contains("3") == true)
+//                    {
+//                        int index = strN.IndexOf("3");
+//                        strN = strN.Remove(index, 1);
+//                        ++ret;
+//                        continue;
+//                    }
 
-                    if (strN.Contains("6") == true)
-                    {
-                        int index = strN.IndexOf("6");
-                        strN = strN.Remove(index, 1);
-                        ++ret;
-                        continue;
-                    }
+//                    if (strN.Contains("6") == true)
+//                    {
+//                        int index = strN.IndexOf("6");
+//                        strN = strN.Remove(index, 1);
+//                        ++ret;
+//                        continue;
+//                    }
 
-                    if (strN.Contains("9") == true)
-                    {
-                        int index = strN.IndexOf("9");
-                        strN = strN.Remove(index, 1);
-                        ++ret;
-                        continue;
-                    }
-                }
-            }
+//                    if (strN.Contains("9") == true)
+//                    {
+//                        int index = strN.IndexOf("9");
+//                        strN = strN.Remove(index, 1);
+//                        ++ret;
+//                        continue;
+//                    }
+//                }
+//            }
 
-            print();
-        }
+//            print();
+//        }
 
-        public void print()
-        {
-            Console.WriteLine(ret);
-        }
-    }
-}
+//        public void print()
+//        {
+//            Console.WriteLine(ret);
+//        }
+//    }
+//}
---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_18420_근손실.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static CodingTestProj.Program;
+
+/*sdsd
+ * Difficulty : 
+ * URL : https://www.acmicpc.net/problem/18429
+  * Time : 
+ */
+
+
+//mobitel
+namespace CodingTestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ss = new Solution();
+            ss.Solve();
+        }
+    }
+
+    public class Solution
+    {
+        public int n;
+        public int ret;
+
+        public string target;
+
+        public void Solve()
+        {
+            ret = 0;
+            n = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; ++i)
+            {
+                string strN = i.ToString();
+
+                while(true)
+                {
+                    if (strN.Contains("3") == false &&
+                        strN.Contains("6") == false &&
+                        strN.Contains("9") == false)
+                        break;
+
+                    if(strN.Contains("3") == true)
+                    {
+                        int index = strN.IndexOf("3");
+                        strN = strN.Remove(index, 1);
+                        ++ret;
+                        continue;
+                    }
+
+                    if (strN.Contains("6") == true)
+                    {
+                        int index = strN.IndexOf("6");
+                        strN = strN.Remove(index, 1);
+                        ++ret;
+                        continue;
+                    }
+
+                    if (strN.Contains("9") == true)
+                    {
+                        int index = strN.IndexOf("9");
+                        strN = strN.Remove(index, 1);
+                        ++ret;
+                        continue;
+                    }
+                }
+            }
+
+            print();
+        }
+
+        public void print()
+        {
+            Console.WriteLine(ret);
+        }
+    }
+}
