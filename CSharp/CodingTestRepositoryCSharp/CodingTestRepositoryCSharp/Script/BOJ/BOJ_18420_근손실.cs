4603593f313d43083775e698092fd0c3c3696785
jonghow <sjh15348@gmail.com>
2024-10-25 23:06:43 +0900

    ProblemSolve_BOJ_18429_근손실
    https://www.acmicpc.net/problem/18429

--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_18420_근손실.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_18420_근손실.cs
@@ -3,8 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using static CodingTestProj.Program;
 
-/*sdsd
- * Difficulty : 
+/*
+ * Difficulty : Middle
  * URL : https://www.acmicpc.net/problem/18429
   * Time : 
  */
@@ -18,65 +18,127 @@ namespace CodingTestProj
         static void Main(string[] args)
         {
             var ss = new Solution();
-            ss.Solve();
+            ss.solve();
         }
     }
 
     public class Solution
     {
-        public int n;
-        public int ret;
+        public int _n;
+        public int _k;
+        public int _declineProtain;
 
-        public string target;
+        public bool[] _isSelected;
+        public int[] _numbers;
 
-        public void Solve()
+        public int _ret;
+
+        public int _permuteCnt;
+        public int _nowWeight;
+
+        public int _dic_index;
+        public Dictionary<int, int> _dicKitVal = new Dictionary<int, int>();
+        public List<KeyValuePair<int, List<int>>> _listPermute = new List<KeyValuePair<int, List<int>>>();
+
+        public void solve()
         {
-            ret = 0;
-            n = Int32.Parse(Console.ReadLine());
+            Init();
 
-            for (int i = 1; i <= n; ++i)
+            string[] input = Console.ReadLine().Split(' ');
+            _n = Int32.Parse(input[0]);
+            _k = Int32.Parse(input[1]);
+
+            _isSelected = new bool[_n];
+            _numbers = new int[_n];
+
+            for (int i = 0; i < _n; ++i)
+                _numbers[i] = i + 1;
+
+            input = Console.ReadLine().Split(' ');
+
+            for (int i = 1; i <= _n; ++i)
+                _dicKitVal.Add(i, Int32.Parse(input[i - 1]));
+
+            int[] arr = new int[_n];
+            Permutation(0, 0, ref arr);
+            MainSolution();
+
+            Print();
+        }
+
+        public void Init()
+        {
+            _n = 0;
+            _k = 0;
+            _declineProtain = 0;
+            _dic_index = 0;
+            _nowWeight = 500;
+        }
+
+        public void Permutation(int _depth, int _pos, ref int[] arr)
+        {
+            if (_depth == (_n))
             {
-                string strN = i.ToString();
+                List<int> _mList = new List<int>();
+                _mList.AddRange(arr);
 
-                while(true)
+                _listPermute.Add(new KeyValuePair<int, List<int>>(_dic_index, _mList));
+
+                //for (int i = 0; i < _mList.Count; ++i)
+                //    Console.Write(_mList[i] + " ");
+
+                //Console.WriteLine();
+
+                ++_dic_index;
+            }
+            else
+            {
+                for (int i = 0; i < _numbers.Length; ++i)
                 {
-                    if (strN.Contains("3") == false &&
-                        strN.Contains("6") == false &&
-                        strN.Contains("9") == false)
-                        break;
-
-                    if(strN.Contains("3") == true)
+                    if (_isSelected[i] == false)
                     {
-                        int index = strN.IndexOf("3");
-                        strN = strN.Remove(index, 1);
-                        ++ret;
-                        continue;
-                    }
-
-                    if (strN.Contains("6") == true)
-                    {
-                        int index = strN.IndexOf("6");
-                        strN = strN.Remove(index, 1);
-                        ++ret;
-                        continue;
-                    }
-
-                    if (strN.Contains("9") == true)
-                    {
-                        int index = strN.IndexOf("9");
-                        strN = strN.Remove(index, 1);
-                        ++ret;
-                        continue;
+                        _isSelected[i] = true;
+                        arr[_depth] = _numbers[i];
+                        Permutation(_depth + 1, _pos, ref arr);
+                        _isSelected[i] = false;
                     }
                 }
             }
-
-            print();
         }
 
-        public void print()
+        public void MainSolution()
         {
-            Console.WriteLine(ret);
+            for (int i = 0; i < _listPermute.Count; ++i)
+            {
+                var val = _listPermute.Find(rhs => rhs.Key == i).Value;
+                if (val == null) continue;
+
+                _nowWeight = 500;
+                bool isFail = false;
+
+                for (int j = 0; j < val.Count; ++j)
+                {
+                    var kitNumb = val[j];
+                    var kitVal = _dicKitVal[kitNumb];
+
+                    _nowWeight = _nowWeight - (_k) + kitVal;
+
+                    if (_nowWeight < 500)
+                    {
+                        isFail = true;
+                        break;
+                    }
+                }
+
+                if (isFail) continue;
+
+                ++_ret;
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_ret);
         }
     }
 }
