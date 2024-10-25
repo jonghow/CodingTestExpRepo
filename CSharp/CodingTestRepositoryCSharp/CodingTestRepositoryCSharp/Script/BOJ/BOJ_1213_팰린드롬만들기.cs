610299cdcb3748a4293459fb7752a311d47d12b6
jonghow <sjh15348@gmail.com>
2024-10-26 01:49:15 +0900

    ProblemFail_BOJ_1213_팰린드롬만들기
    https://www.acmicpc.net/problem/1213

---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1213_팰린드롬만들기.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static CodingTestProj.Program;
+
+/*
+ * Difficulty : 
+ * URL : https://www.acmicpc.net/problem/1213
+  * Time : 
+ */
+
+namespace CodingTestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ss = new Solution();
+            ss.solve();
+        }
+    }
+
+    public class Solution
+    {
+        public string _ret;
+        public string _wrongRet;
+
+        public void solve()
+        {
+            _wrongRet = "I'm Sorry Hansoo";
+
+            Init();
+            string input = Console.ReadLine();
+            if(input.Length == 0)
+            {
+                _ret = _wrongRet;
+                Print();
+                return;
+            }
+
+            char[] chr = input.ToCharArray();
+
+            Array.Sort(chr);
+
+            string _newInput = new string(chr);
+            string _s1 = string.Empty;
+            string _s2 = string.Empty;
+            bool _isCenter = false;
+            List<char> _chr =  new List<char>();
+
+            //BBAACDAA
+            while (_newInput.Length > 0)
+            {
+                if(_newInput.Length % 2 == 0) // 짝수
+                {
+                    if (_newInput[0] != _newInput[1])
+                    { 
+                        // 짝수인데 다르면 실패
+                        _isCenter = false;
+                        _s1 = "";
+                        _s2 = "";
+                        break;
+                    }
+                }
+
+                if (_newInput.Length == 1)
+                {
+                    _isCenter = true;
+                    _chr.Add(_newInput[0]);
+                    _newInput = _newInput.Remove(0, 1);
+                    break;
+                }
+
+                if ((_newInput[0] != _newInput[1]) && _newInput.Length % 2 == 1)
+                {
+                    if( _isCenter == true) 
+                        break;
+
+                    _isCenter = true;
+                    _chr.Add(_newInput[0]);
+                    _newInput = _newInput.Remove(0, 1);
+                    continue;
+                }
+
+                char _a = _newInput[0];
+                _newInput = _newInput.Remove(0, 1);
+
+                char _b = _newInput[0];
+                _newInput = _newInput.Remove(0, 1);
+
+                _s1 += _a;
+                _s2 += _b;
+            }
+
+            if ((_s1 == "" && _s2 == "") == false && (_s1 == _s2))
+            {
+                char[] reverse_s2 = _s2.ToCharArray();
+                Array.Reverse(reverse_s2);
+                string _new_s2 = new string(reverse_s2);
+
+                if (_isCenter)
+                    _newInput = _chr[0].ToString();
+
+                _ret = _s1 + _newInput + _new_s2;
+            }
+            else
+                _ret = _wrongRet;
+
+            Print();
+        }
+
+        public void Init()
+        {
+        }
+        public void Print()
+        {
+            Console.WriteLine(_ret);
+        }
+    }
+}
--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_18420_근손실.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_18420_근손실.cs
@@ -1,144 +1,144 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using static CodingTestProj.Program;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using static CodingTestProj.Program;
 
-/*
- * Difficulty : Middle
- * URL : https://www.acmicpc.net/problem/18429
-  * Time : 
- */
+///*
+// * Difficulty : Middle
+// * URL : https://www.acmicpc.net/problem/18429
+//  * Time : 
+// */
 
 
-//mobitel
-namespace CodingTestProj
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var ss = new Solution();
-            ss.solve();
-        }
-    }
+////mobitel
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            var ss = new Solution();
+//            ss.solve();
+//        }
+//    }
 
-    public class Solution
-    {
-        public int _n;
-        public int _k;
-        public int _declineProtain;
+//    public class Solution
+//    {
+//        public int _n;
+//        public int _k;
+//        public int _declineProtain;
 
-        public bool[] _isSelected;
-        public int[] _numbers;
+//        public bool[] _isSelected;
+//        public int[] _numbers;
 
-        public int _ret;
+//        public int _ret;
 
-        public int _permuteCnt;
-        public int _nowWeight;
+//        public int _permuteCnt;
+//        public int _nowWeight;
 
-        public int _dic_index;
-        public Dictionary<int, int> _dicKitVal = new Dictionary<int, int>();
-        public List<KeyValuePair<int, List<int>>> _listPermute = new List<KeyValuePair<int, List<int>>>();
+//        public int _dic_index;
+//        public Dictionary<int, int> _dicKitVal = new Dictionary<int, int>();
+//        public List<KeyValuePair<int, List<int>>> _listPermute = new List<KeyValuePair<int, List<int>>>();
 
-        public void solve()
-        {
-            Init();
+//        public void solve()
+//        {
+//            Init();
 
-            string[] input = Console.ReadLine().Split(' ');
-            _n = Int32.Parse(input[0]);
-            _k = Int32.Parse(input[1]);
+//            string[] input = Console.ReadLine().Split(' ');
+//            _n = Int32.Parse(input[0]);
+//            _k = Int32.Parse(input[1]);
 
-            _isSelected = new bool[_n];
-            _numbers = new int[_n];
+//            _isSelected = new bool[_n];
+//            _numbers = new int[_n];
 
-            for (int i = 0; i < _n; ++i)
-                _numbers[i] = i + 1;
+//            for (int i = 0; i < _n; ++i)
+//                _numbers[i] = i + 1;
 
-            input = Console.ReadLine().Split(' ');
+//            input = Console.ReadLine().Split(' ');
 
-            for (int i = 1; i <= _n; ++i)
-                _dicKitVal.Add(i, Int32.Parse(input[i - 1]));
+//            for (int i = 1; i <= _n; ++i)
+//                _dicKitVal.Add(i, Int32.Parse(input[i - 1]));
 
-            int[] arr = new int[_n];
-            Permutation(0, 0, ref arr);
-            MainSolution();
+//            int[] arr = new int[_n];
+//            Permutation(0, 0, ref arr);
+//            MainSolution();
 
-            Print();
-        }
+//            Print();
+//        }
 
-        public void Init()
-        {
-            _n = 0;
-            _k = 0;
-            _declineProtain = 0;
-            _dic_index = 0;
-            _nowWeight = 500;
-        }
+//        public void Init()
+//        {
+//            _n = 0;
+//            _k = 0;
+//            _declineProtain = 0;
+//            _dic_index = 0;
+//            _nowWeight = 500;
+//        }
 
-        public void Permutation(int _depth, int _pos, ref int[] arr)
-        {
-            if (_depth == (_n))
-            {
-                List<int> _mList = new List<int>();
-                _mList.AddRange(arr);
+//        public void Permutation(int _depth, int _pos, ref int[] arr)
+//        {
+//            if (_depth == (_n))
+//            {
+//                List<int> _mList = new List<int>();
+//                _mList.AddRange(arr);
 
-                _listPermute.Add(new KeyValuePair<int, List<int>>(_dic_index, _mList));
+//                _listPermute.Add(new KeyValuePair<int, List<int>>(_dic_index, _mList));
 
-                //for (int i = 0; i < _mList.Count; ++i)
-                //    Console.Write(_mList[i] + " ");
+//                //for (int i = 0; i < _mList.Count; ++i)
+//                //    Console.Write(_mList[i] + " ");
 
-                //Console.WriteLine();
+//                //Console.WriteLine();
 
-                ++_dic_index;
-            }
-            else
-            {
-                for (int i = 0; i < _numbers.Length; ++i)
-                {
-                    if (_isSelected[i] == false)
-                    {
-                        _isSelected[i] = true;
-                        arr[_depth] = _numbers[i];
-                        Permutation(_depth + 1, _pos, ref arr);
-                        _isSelected[i] = false;
-                    }
-                }
-            }
-        }
+//                ++_dic_index;
+//            }
+//            else
+//            {
+//                for (int i = 0; i < _numbers.Length; ++i)
+//                {
+//                    if (_isSelected[i] == false)
+//                    {
+//                        _isSelected[i] = true;
+//                        arr[_depth] = _numbers[i];
+//                        Permutation(_depth + 1, _pos, ref arr);
+//                        _isSelected[i] = false;
+//                    }
+//                }
+//            }
+//        }
 
-        public void MainSolution()
-        {
-            for (int i = 0; i < _listPermute.Count; ++i)
-            {
-                var val = _listPermute.Find(rhs => rhs.Key == i).Value;
-                if (val == null) continue;
+//        public void MainSolution()
+//        {
+//            for (int i = 0; i < _listPermute.Count; ++i)
+//            {
+//                var val = _listPermute.Find(rhs => rhs.Key == i).Value;
+//                if (val == null) continue;
 
-                _nowWeight = 500;
-                bool isFail = false;
+//                _nowWeight = 500;
+//                bool isFail = false;
 
-                for (int j = 0; j < val.Count; ++j)
-                {
-                    var kitNumb = val[j];
-                    var kitVal = _dicKitVal[kitNumb];
+//                for (int j = 0; j < val.Count; ++j)
+//                {
+//                    var kitNumb = val[j];
+//                    var kitVal = _dicKitVal[kitNumb];
 
-                    _nowWeight = _nowWeight - (_k) + kitVal;
+//                    _nowWeight = _nowWeight - (_k) + kitVal;
 
-                    if (_nowWeight < 500)
-                    {
-                        isFail = true;
-                        break;
-                    }
-                }
+//                    if (_nowWeight < 500)
+//                    {
+//                        isFail = true;
+//                        break;
+//                    }
+//                }
 
-                if (isFail) continue;
+//                if (isFail) continue;
 
-                ++_ret;
-            }
-        }
+//                ++_ret;
+//            }
+//        }
 
-        public void Print()
-        {
-            Console.WriteLine(_ret);
-        }
-    }
-}
+//        public void Print()
+//        {
+//            Console.WriteLine(_ret);
+//        }
+//    }
+//}
