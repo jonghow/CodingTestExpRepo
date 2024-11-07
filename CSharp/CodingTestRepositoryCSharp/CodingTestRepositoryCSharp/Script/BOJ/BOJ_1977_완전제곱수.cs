2a6ed1e1f00e9210d2554dede9d3053a389408ea
jonghow <sjh15348@gmail.com>
2024-11-07 23:59:12 +0900

    ProblemFail_BOJ_15649_N과M_1
    https://www.acmicpc.net/problem/15649

--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1977_완전제곱수.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1977_완전제곱수.cs
@@ -1,73 +1,73 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Xml.Linq;
-using static CodingTestProj.Program;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Xml.Linq;
+//using static CodingTestProj.Program;
 
-///*
-// * Difficulty : Easy
-// * URL : https://www.acmicpc.net/problem/1977
-//  * Time : 8m 8s
-// */
+/////*
+//// * Difficulty : Easy
+//// * URL : https://www.acmicpc.net/problem/1977
+////  * Time : 8m 8s
+//// */
 
-namespace CodingTestProj
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Solution solu = new Solution();
-            solu.solve();
-        }
-    }
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Solution solu = new Solution();
+//            solu.solve();
+//        }
+//    }
 
-    public class Solution
-    {
-        public List<int> _retList = new List<int>(); // 완전제곱수 모음.
+//    public class Solution
+//    {
+//        public List<int> _retList = new List<int>(); // 완전제곱수 모음.
 
-        public int _retSum;
-        public int _retSmall;
+//        public int _retSum;
+//        public int _retSmall;
 
-        public int _n;
-        public int _m;
+//        public int _n;
+//        public int _m;
 
-        public void solve()
-        {
-            _retSmall = int.MaxValue;
-            _n = int.Parse(Console.ReadLine());
-            _m = int.Parse(Console.ReadLine());
+//        public void solve()
+//        {
+//            _retSmall = int.MaxValue;
+//            _n = int.Parse(Console.ReadLine());
+//            _m = int.Parse(Console.ReadLine());
 
-            int idx = 0;
-            while (true)
-            {
-                int _pow = (int)Math.Pow(idx, 2);
+//            int idx = 0;
+//            while (true)
+//            {
+//                int _pow = (int)Math.Pow(idx, 2);
 
-                if (_pow > _m)
-                    break;
+//                if (_pow > _m)
+//                    break;
 
-                if(_pow >= _n && _pow <= _m)
-                {
-                    _retList.Add(_pow);
-                    _retSum += _pow;
+//                if(_pow >= _n && _pow <= _m)
+//                {
+//                    _retList.Add(_pow);
+//                    _retSum += _pow;
 
-                    if (_retSmall >= _pow)
-                        _retSmall = _pow;
+//                    if (_retSmall >= _pow)
+//                        _retSmall = _pow;
 
 
-                }
+//                }
 
-                idx++;
-            }
+//                idx++;
+//            }
 
-            if(_retList.Count > 0 )
-            {
-                Console.WriteLine(_retSum);
-                Console.WriteLine(_retSmall);
-            }
-            else
-            {
-                Console.WriteLine(-1);
-            }
-        }
-    }
-}
+//            if(_retList.Count > 0 )
+//            {
+//                Console.WriteLine(_retSum);
+//                Console.WriteLine(_retSmall);
+//            }
+//            else
+//            {
+//                Console.WriteLine(-1);
+//            }
+//        }
+//    }
+//}
