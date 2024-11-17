e84e4cbfef4b9372abc2f90cb8cdb0c459fa4d70
jonghow <sjh15348@gmail.com>
2024-11-17 23:50:11 +0900

    ProblemSolve_BOJ_15651_N과M_3
    https://www.acmicpc.net/problem/15651

--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_15649_N과M_2.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_15649_N과M_2.cs
@@ -1,83 +1,83 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Xml.Linq;
-using static CodingTestProj.Program;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Xml.Linq;
+//using static CodingTestProj.Program;
 
-///*
-// * Difficulty : Easy
-// * URL : https://www.acmicpc.net/problem/15650
-//  * Time : 10m Under
-// */
+/////*
+//// * Difficulty : Easy
+//// * URL : https://www.acmicpc.net/problem/15650
+////  * Time : 10m Under
+//// */
 
-namespace CodingTestProj
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Solution solu = new Solution();
-            solu.solve();
-        }
-    }
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Solution solu = new Solution();
+//            solu.solve();
+//        }
+//    }
 
-    public class Solution
-    {
-        public const int _max = 9;
-        public int _n;
-        public int _m;
+//    public class Solution
+//    {
+//        public const int _max = 9;
+//        public int _n;
+//        public int _m;
 
-        public int[] _per;
-        public int[] _arr;
+//        public int[] _per;
+//        public int[] _arr;
 
-        public int _curPos;
+//        public int _curPos;
 
-        public bool[] _isVisit;
+//        public bool[] _isVisit;
 
-        public void solve()
-        {
-            string[] _input = Console.ReadLine().Split(' ');
-            _n = int.Parse(_input[0]);
-            _m = int.Parse(_input[1]);
+//        public void solve()
+//        {
+//            string[] _input = Console.ReadLine().Split(' ');
+//            _n = int.Parse(_input[0]);
+//            _m = int.Parse(_input[1]);
 
-            _isVisit = new bool[_max];
-            _arr = new int[_max];
-            _per = new int[_max];
+//            _isVisit = new bool[_max];
+//            _arr = new int[_max];
+//            _per = new int[_max];
 
-            for (int i = 0; i < 9; ++i)
-                _arr[i] = i + 1;
+//            for (int i = 0; i < 9; ++i)
+//                _arr[i] = i + 1;
 
-            dfs(0,0);
-        }
+//            dfs(0,0);
+//        }
 
-        public void dfs(int _cnt, int pos)
-        {
-            if (_cnt >= _m)
-            {
-                for(int i = 0; i < _cnt; ++i)
-                {
-                    Console.Write(_per[i]);
+//        public void dfs(int _cnt, int pos)
+//        {
+//            if (_cnt >= _m)
+//            {
+//                for(int i = 0; i < _cnt; ++i)
+//                {
+//                    Console.Write(_per[i]);
 
-                    if (i != _cnt-1)
-                        Console.Write(" ");
-                }
+//                    if (i != _cnt-1)
+//                        Console.Write(" ");
+//                }
 
-                Console.WriteLine();
-            }
-            else
-            {
+//                Console.WriteLine();
+//            }
+//            else
+//            {
 
-                for (int i = pos; i < _n; ++i)
-                {
-                    if (_isVisit[i] == false)
-                    {
-                        _isVisit[i] = true;
-                        _per[_cnt] = _arr[i];
-                        dfs(_cnt + 1, i +1);
-                        _isVisit[i] = false;
-                    }
-                }
-            }
-        }
-    }
-}
+//                for (int i = pos; i < _n; ++i)
+//                {
+//                    if (_isVisit[i] == false)
+//                    {
+//                        _isVisit[i] = true;
+//                        _per[_cnt] = _arr[i];
+//                        dfs(_cnt + 1, i +1);
+//                        _isVisit[i] = false;
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
