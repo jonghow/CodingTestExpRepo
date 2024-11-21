e7cc759ebdb65efb383fa9f61f88537dec05afcf
jonghow <sjh15348@gmail.com>
2024-11-21 22:29:30 +0900

    ProblemSolve_BOJ_15654_N과M_5
    https://www.acmicpc.net/problem/15654

---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_15654_N과M_5.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/////*
+//// * Difficulty : Easy
+//// * URL : https://www.acmicpc.net/problem/15654
+////  * Time : 1h over
+//// */
+///
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.solve();
+        }
+    }
+
+    public class Solution
+    {
+        public const int _max = 9;
+        public const int _valmax = 10001;
+        public int _n;
+        public int _m;
+        public int[] _arr;
+        public int[] _per;
+
+        public bool[] _isVisit;
+
+        public HashSet<string> _ret;
+
+        public void solve()
+        {
+            _ret = new HashSet<string>();
+            string[] _in = Console.ReadLine().Split(' ');
+            _n = int.Parse(_in[0].ToString());
+            _m = int.Parse(_in[1].ToString());
+            _isVisit = new bool[_valmax];
+
+            _in = Console.ReadLine().Split(' ');
+            _arr = new int[_max];
+
+            for (int i = 0; i < _in.Length; ++i)
+                _arr[i] = Int32.Parse(_in[i]);
+
+            _per = new int[_m];
+
+            List<int> _temp = new List<int>(_arr);
+            _temp.Sort(Sorted);
+            _arr = _temp.ToArray();
+
+            BT(ref _arr, ref _per, 0, 0);
+
+            int index = 0;
+            foreach (string s in _ret)
+            {
+                Console.Write(s);
+
+                if (index != _ret.Count - 1)
+                    Console.WriteLine();
+            }
+        }
+
+        public int Sorted(string _item1, string _item2)
+        {
+            bool _isEmptyitem1 = _item1 == null || _item1.Length == 0 ? true : false;
+            bool _isEmptyitem2 = _item2 == null || _item2.Length == 0 ? true : false;
+
+            if (_item1 == null)
+                _item1 = " ";
+
+            if (_item2 == null)
+                _item2 = " ";
+
+            if (_isEmptyitem1 != _isEmptyitem2)
+                return _isEmptyitem1.CompareTo(_isEmptyitem2);
+
+            return _item1.CompareTo(_item2);
+        }
+
+        public int Sorted(int _item1, int _item2)
+        {
+            bool _isEmptyitem1 = _item1 == 0 ? true : false;
+            bool _isEmptyitem2 = _item2 == 0 ? true : false;
+
+            if (_isEmptyitem1 != _isEmptyitem2)
+                return _isEmptyitem1.CompareTo(_isEmptyitem2);
+
+            return _item1.CompareTo(_item2);
+        }
+
+        public void BT(ref int[] _arr, ref int[] _per, int _cnt, int _pos)
+        {
+            if (_cnt >= _m)
+            {
+                string _st = string.Empty;
+
+                for (int i = 0; i < _cnt; ++i)
+                {
+                    _st += _per[i];
+
+                    if (i != _cnt - 1)
+                        _st += " ";
+                }
+
+                if (_ret.Contains(_st) == false)
+                    _ret.Add(_st);
+            }
+            else
+            {
+                for (int i = 0; i < _n; ++i)
+                {
+                    if (_isVisit[Int32.Parse(_arr[i].ToString())] == false)
+                    {
+                        _isVisit[Int32.Parse(_arr[i].ToString())] = true;
+                        _per[_pos] = _arr[i];
+                        BT(ref _arr, ref _per, _cnt + 1, _pos + 1);
+
+                        _isVisit[Int32.Parse(_arr[i].ToString())] = false;
+                    }
+                }
+            }
+        }
+    }
+}
+
--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_21735_눈덩이굴리기.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_21735_눈덩이굴리기.cs
@@ -1,69 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-/////*
-//// * Difficulty : 
-//// * URL : https://www.acmicpc.net/problem/21735
-////  * Time : 
-//// */
+///////*
+////// * Difficulty : Middle
+////// * URL : https://www.acmicpc.net/problem/21735
+//////  * Time : 1h over
+////// */
 
-namespace Test
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Solution solution = new Solution();
-            solution.solve();
-        }
-    }
+//namespace Test
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Solution solution = new Solution();
+//            solution.solve();
+//        }
+//    }
 
-    public class Solution
-    {
-        public int _n;
-        public int _m;
-        public int[] _arr;
-        public int[] _per;
+//    public class Solution
+//    {
+//        public int _n;
+//        public int _m;
+//        public int[] _arr;
+//        public int[] _per;
 
-        public float _ret = 0;
+//        public float _ret = 0;
 
-        public void solve()
-        {
-            _ret = float.MinValue;
-            string[] _in = Console.ReadLine().Split(' ');
-            _n = int.Parse(_in[0].ToString());
-            _m = int.Parse(_in[1].ToString());
+//        public void solve()
+//        {
+//            _ret = float.MinValue;
+//            string[] _in = Console.ReadLine().Split(' ');
+//            _n = int.Parse(_in[0].ToString());
+//            _m = int.Parse(_in[1].ToString());
 
-            _in = Console.ReadLine().Split(' ');
-            _arr = new int[_in.Length];
+//            _in = Console.ReadLine().Split(' ');
+//            _arr = new int[_in.Length];
 
-            for (int i = 0; i < _in.Length; ++i)
-                _arr[i] = int.Parse(_in[i].ToString());
+//            for (int i = 0; i < _in.Length; ++i)
+//                _arr[i] = int.Parse(_in[i].ToString());
 
-            dfs(0, 0, 1f, false);
+//            dfs(0, 0, 1f, false);
 
-            Console.Write(_ret);
-        }
+//            Console.Write(_ret);
+//        }
 
-        public void dfs(int cnt, int pos, float calc, bool isThrow)
-        {
-            if (cnt >= _m || pos >= _n)
-            {
-                if (_ret < calc)
-                    _ret = calc;
-            }
-            else
-            {
-                    // 1번으로 수행했을 때, 
-                    dfs(cnt + 1, pos + 1, calc + _arr[pos], false);
+//        public void dfs(int cnt, int pos, float calc, bool isThrow)
+//        {
+//            if (cnt >= _m || pos >= _n)
+//            {
+//                if (_ret < calc)
+//                    _ret = calc;
+//            }
+//            else
+//            {
+//                    // 1번으로 수행했을 때, 
+//                    dfs(cnt + 1, pos + 1, calc + _arr[pos], false);
 
-                    // 2번으로 수행했을 때,
-                    float calcThrow = calc * 0.5f;
-                    int valBasedLength = ((pos + 1) >= _arr.Length) == true ? 0 : _arr[pos + 1];
-                    dfs(cnt + 1, pos + 2,  (float)Math.Floor(calcThrow + valBasedLength), true);
-            }
-        }
-    }
-}
+//                    // 2번으로 수행했을 때,
+//                    float calcThrow = calc * 0.5f;
+//                    int valBasedLength = ((pos + 1) >= _arr.Length) == true ? 0 : _arr[pos + 1];
+//                    dfs(cnt + 1, pos + 2,  (float)Math.Floor(calcThrow + valBasedLength), true);
+//            }
+//        }
+//    }
+//}
 
