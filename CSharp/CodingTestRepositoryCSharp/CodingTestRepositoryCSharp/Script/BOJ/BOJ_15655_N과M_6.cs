2d26d9fd287b1aff7d58adf5b48cb1c04e94e0fb
jonghow <sjh15348@gmail.com>
2024-12-10 23:45:12 +0900

    ProblemFail_BOJ_2992_크면서_작은_수
    https://www.acmicpc.net/problem/2992

--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_15655_N과M_6.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_15655_N과M_6.cs
@@ -1,96 +1,96 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-/////*
-//// * Difficulty : Easy
-//// * URL : https://www.acmicpc.net/problem/15655
-////  * Time : 11m 
-//// */
-///
-namespace CodingTestProj
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Solution solution = new Solution();
-            solution.solve();
-        }
-    }
+///////*
+////// * Difficulty : Easy
+////// * URL : https://www.acmicpc.net/problem/15655
+//////  * Time : 11m 
+////// */
+/////
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Solution solution = new Solution();
+//            solution.solve();
+//        }
+//    }
 
-    public class Solution
-    {
-        public const int _max = 9;
-        public const int _valmax = 10001;
-        public int _n;
-        public int _m;
-        public int[] _arr;
-        public int[] _per;
+//    public class Solution
+//    {
+//        public const int _max = 9;
+//        public const int _valmax = 10001;
+//        public int _n;
+//        public int _m;
+//        public int[] _arr;
+//        public int[] _per;
 
-        public bool[] _isVisit;
+//        public bool[] _isVisit;
 
-        public HashSet<string> _hs;
+//        public HashSet<string> _hs;
 
-        public StringBuilder _sb;
-        public void solve()
-        {
-            _sb = new StringBuilder();
-            _hs = new HashSet<string>();
+//        public StringBuilder _sb;
+//        public void solve()
+//        {
+//            _sb = new StringBuilder();
+//            _hs = new HashSet<string>();
 
-            string[] _input = Console.ReadLine().Split(' ');
-            _n = int.Parse(_input[0]);
-            _m = int.Parse(_input[1]);
+//            string[] _input = Console.ReadLine().Split(' ');
+//            _n = int.Parse(_input[0]);
+//            _m = int.Parse(_input[1]);
 
-            _input = Console.ReadLine().Split(' ');
+//            _input = Console.ReadLine().Split(' ');
 
-            _arr = new int[_n];
-            _per = new int[_m];
+//            _arr = new int[_n];
+//            _per = new int[_m];
 
-            for(int i = 0; i < _input.Length; ++i)
-            {
-                _arr[i] = int.Parse(_input[i]);
-            }
+//            for(int i = 0; i < _input.Length; ++i)
+//            {
+//                _arr[i] = int.Parse(_input[i]);
+//            }
 
-            Array.Sort(_arr);
+//            Array.Sort(_arr);
 
-            BT(0, ref _per, -1);
+//            BT(0, ref _per, -1);
 
-            Console.Write(_sb.ToString());
-        }
+//            Console.Write(_sb.ToString());
+//        }
 
-        public void BT(int _cnt, ref int[] _c, int _prev)
-        {
-            if(_cnt == _m)
-            {
-                string _in = string.Empty;
+//        public void BT(int _cnt, ref int[] _c, int _prev)
+//        {
+//            if(_cnt == _m)
+//            {
+//                string _in = string.Empty;
 
-                for(int i = 0; i < _cnt; ++i)
-                {
-                    _in += _c[i];
+//                for(int i = 0; i < _cnt; ++i)
+//                {
+//                    _in += _c[i];
 
-                    if (i != _cnt - 1)
-                        _in += ' ';
-                }
+//                    if (i != _cnt - 1)
+//                        _in += ' ';
+//                }
 
-                if (_hs.Contains(_in) == false)
-                {
-                    _hs.Add(_in);
-                    _sb.Append(_in);
-                    _sb.AppendLine();
-                }
-            }
-            else
-            {
-                for(int i = _cnt; i < _n; ++i)
-                {
-                    if (_prev >= _arr[i]) continue;
+//                if (_hs.Contains(_in) == false)
+//                {
+//                    _hs.Add(_in);
+//                    _sb.Append(_in);
+//                    _sb.AppendLine();
+//                }
+//            }
+//            else
+//            {
+//                for(int i = _cnt; i < _n; ++i)
+//                {
+//                    if (_prev >= _arr[i]) continue;
 
-                    _c[_cnt] = _arr[i];
-                    BT(_cnt + 1, ref _c, _arr[i]);
-                }
-            }
-        }
-    }
-}
+//                    _c[_cnt] = _arr[i];
+//                    BT(_cnt + 1, ref _c, _arr[i]);
+//                }
+//            }
+//        }
+//    }
+//}
 
--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_2992_크면서_작은_수.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_2992_크면서_작은_수.cs
@@ -1,91 +1,91 @@
-﻿//using System;
-//using System.Collections;
-//using System.Collections.Generic;
-//using System.Xml.Linq;
-//using static CodingTestProj.Program;
-//using System.Text;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using static CodingTestProj.Program;
+using System.Text;
 
-/////*
-//// * Difficulty : 
-//// * URL : https://www.acmicpc.net/problem/2992
-////  * Time : 
-//// */
+///*
+// * Difficulty : 
+// * URL : https://www.acmicpc.net/problem/2992
+//  * Time : 
+// */
 
-//namespace CodingTestProj
-//{
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Solution solu = new Solution();
-//            solu.solve();
-//        }
-//    }
+namespace CodingTestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solu = new Solution();
+            solu.solve();
+        }
+    }
 
-//    public class Solution
-//    {
-//        public const int _max = 8;
-//        public int _n;
-//        public int _m;
-//        public int[] _arr;
-//        public bool[] _visit;
+    public class Solution
+    {
+        public const int _max = 8;
+        public int _n;
+        public int _m;
+        public int[] _arr;
+        public bool[] _visit;
 
-//        public StringBuilder _sb;
-//        public HashSet<string> _hs;
-//        public void solve()
-//        {
-//            _hs = new HashSet<string>();
-//            string[] _input = Console.ReadLine().Split(' ');
+        public StringBuilder _sb;
+        public HashSet<string> _hs;
+        public void solve()
+        {
+            _hs = new HashSet<string>();
+            string[] _input = Console.ReadLine().Split(' ');
 
-//            _n = int.Parse(_input[0]);
-//            _m = int.Parse(_input[1]);
+            _n = int.Parse(_input[0]);
+            _m = int.Parse(_input[1]);
 
-//            _input = Console.ReadLine().Split(' ');
-//            _visit = new bool[_max];
-//            _sb = new StringBuilder();
-//            _arr = new int[_n];
-//            int[] _c = new int[_m];
+            _input = Console.ReadLine().Split(' ');
+            _visit = new bool[_max];
+            _sb = new StringBuilder();
+            _arr = new int[_n];
+            int[] _c = new int[_m];
 
-//            for (int i = 0; i < _arr.Length; ++i)
-//                _arr[i] = int.Parse(_input[i]);
+            for (int i = 0; i < _arr.Length; ++i)
+                _arr[i] = int.Parse(_input[i]);
 
-//            Array.Sort(_arr);
+            Array.Sort(_arr);
 
-//            BT(0, ref _c, -1);
-//            Console.Write(_sb.ToString());
-//        }
+            BT(0, ref _c, -1);
+            Console.Write(_sb.ToString());
+        }
 
-//        public void BT(int pos, ref int[] _c, int _Prev)
-//        {
-//            if(pos == _m)
-//            {
-//                string _s = string.Empty;
+        public void BT(int pos, ref int[] _c, int _Prev)
+        {
+            if (pos == _m)
+            {
+                string _s = string.Empty;
 
-//                for(int i = 0; i < _c.Length; ++i)
-//                {
-//                    _s += _c[i];
+                for (int i = 0; i < _c.Length; ++i)
+                {
+                    _s += _c[i];
 
-//                    if (i != _c.Length - 1)
-//                        _s += ' ';
-//                }
+                    if (i != _c.Length - 1)
+                        _s += ' ';
+                }
 
-//                if(!_hs.Contains(_s))
-//                {
-//                    _hs.Add(_s);
-//                    _sb.Append(_s);
-//                    _sb.AppendLine();
-//                }
-//            }
-//            else
-//            {
-//                for(int i =0; i< _n; ++i)
-//                {
-//                    if (_arr[i] < _Prev) continue;
+                if (!_hs.Contains(_s))
+                {
+                    _hs.Add(_s);
+                    _sb.Append(_s);
+                    _sb.AppendLine();
+                }
+            }
+            else
+            {
+                for (int i = 0; i < _n; ++i)
+                {
+                    if (_arr[i] < _Prev) continue;
 
-//                    _c[pos] = _arr[i];
-//                    BT(pos + 1, ref _c, _arr[i]);
-//                }
-//            }
-//        }
-//    }
-//}
+                    _c[pos] = _arr[i];
+                    BT(pos + 1, ref _c, _arr[i]);
+                }
+            }
+        }
+    }
+}
