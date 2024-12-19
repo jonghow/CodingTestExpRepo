69ad3a6294ef4e8ddcafa0ae0f6fac5fc0896582
jonghow <sjh15348@gmail.com>
2024-12-19 23:48:04 +0900

    ProblemFail_BOJ_2003_수들의합2
    https://www.acmicpc.net/problem/2003

---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_2003_수들의합2.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using static CodingTestProj.Program;
+using System.Text;
+
+///*
+// * Difficulty : 
+// * URL : https://www.acmicpc.net/problem/2003
+//  * Time : 
+// */
+
+namespace CodingTestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solu = new Solution();
+            solu.solve();
+        }
+    }
+
+    public class Solution
+    {
+        int _n;
+        int _m;
+
+        int _left;
+        int _right;
+
+        int[] _arr;
+        public void solve()
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            _n = int.Parse(input[0]);
+            _m = int.Parse(input[1]);
+
+            _arr = new int[_n];
+
+            //Array.Sort(_arr);
+
+            input = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < input.Length; i++)
+                _arr[i] = int.Parse(input[i]);
+
+            _left = 0;
+            _right = _arr.Length - 1;
+
+            int _rightMax = _arr.Length -1 ;
+
+            int _StandardLeft = 0;
+            int _StandardRight = 0;
+
+            int _addLeft = 0;
+            int _addRight = 0;
+
+            int _ret = 0;
+
+            while( _left <= (_rightMax * 0.5))
+            {
+                _StandardLeft = _left;
+                _StandardRight= _right;
+
+                _addLeft = 0;
+                _addRight = 0;
+
+                bool _isSuccess = false;
+
+                while (_StandardLeft <= (_rightMax* 0.5))
+                {
+                    _addLeft += _arr[_StandardLeft];
+
+                    if (_addLeft == _m)
+                    {
+                        ++_ret;
+                        _isSuccess = true;
+                        break;
+                    }
+                    else if (_addLeft > _m)
+                        break;
+                    else
+                        ++_StandardLeft;
+                }
+
+                if (_isSuccess && _StandardLeft == _StandardRight)
+                {
+                    ++_left;
+                    --_right;
+                    continue;
+                }
+
+                while (_StandardRight >= (_rightMax * 0.5))
+                {
+                    _addRight += _arr[_StandardRight];
+
+                    if (_addRight == _m)
+                    {
+                        ++_ret;
+                        break;
+                    }
+                    else if (_addRight > _m)
+                        break;
+                    else
+                        --_StandardRight;
+                }
+
+                ++_left;
+                --_right;
+            }
+
+            Console.WriteLine(_ret);
+        }
+    }
+}
--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_2018_수들의합5.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_2018_수들의합5.cs
@@ -1,100 +1,100 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Xml.Linq;
-using static CodingTestProj.Program;
-using System.Text;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Xml.Linq;
+//using static CodingTestProj.Program;
+//using System.Text;
 
-///*
-// * Difficulty : Easy
-// * URL : https://www.acmicpc.net/problem/2018
-//  * Time : 30m
-// */
+/////*
+//// * Difficulty : Easy
+//// * URL : https://www.acmicpc.net/problem/2018
+////  * Time : 30m
+//// */
 
-namespace CodingTestProj
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Solution solu = new Solution();
-            solu.solve();
-        }
-    }
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Solution solu = new Solution();
+//            solu.solve();
+//        }
+//    }
 
-    public class Solution
-    {
-        public int _n;
-        public int _left;
-        public int _right;
-        public int _ret;
+//    public class Solution
+//    {
+//        public int _n;
+//        public int _left;
+//        public int _right;
+//        public int _ret;
 
-        public int[] _arr;
+//        public int[] _arr;
 
-        public void solve()
-        {
-            _n = int.Parse(Console.ReadLine());
-            _left = 0;
-            _right = _n-1;
-            _ret = 0;
-            _arr = new int[_n];
+//        public void solve()
+//        {
+//            _n = int.Parse(Console.ReadLine());
+//            _left = 0;
+//            _right = _n-1;
+//            _ret = 0;
+//            _arr = new int[_n];
 
-            for(int i= 0; i < _n; i++)
-            {
-                _arr[i] = i+1;
-            }
+//            for(int i= 0; i < _n; i++)
+//            {
+//                _arr[i] = i+1;
+//            }
 
-            int _leftAdd = 0;
-            int _rightAdd = 0;
+//            int _leftAdd = 0;
+//            int _rightAdd = 0;
 
-            int _standardLeft = 0;
-            int _standardRight = 0;
-            // 기준 인덱스
+//            int _standardLeft = 0;
+//            int _standardRight = 0;
+//            // 기준 인덱스
 
-            while(_left < (_n * 0.5))
-            {
-                _standardLeft = _left;
-                _standardRight = _right;
-                _leftAdd = 0;
-                _rightAdd = 0;
+//            while(_left < (_n * 0.5))
+//            {
+//                _standardLeft = _left;
+//                _standardRight = _right;
+//                _leftAdd = 0;
+//                _rightAdd = 0;
 
-                while (_standardLeft < (_n * 0.5))
-                {
-                    _leftAdd += _arr[_standardLeft];
+//                while (_standardLeft < (_n * 0.5))
+//                {
+//                    _leftAdd += _arr[_standardLeft];
 
-                    if (_leftAdd == _n)
-                    {
-                        ++_ret;
-                        break;
-                    }
-                    else if (_leftAdd > _n)
-                        break;
-                    else
-                        ++_standardLeft;
-                }
-                // Left
+//                    if (_leftAdd == _n)
+//                    {
+//                        ++_ret;
+//                        break;
+//                    }
+//                    else if (_leftAdd > _n)
+//                        break;
+//                    else
+//                        ++_standardLeft;
+//                }
+//                // Left
 
-                while(_standardRight > (_left))
-                {
-                    _rightAdd += _arr[_standardRight];
+//                while(_standardRight > (_left))
+//                {
+//                    _rightAdd += _arr[_standardRight];
 
-                    if (_rightAdd == _n)
-                    {
-                        ++_ret;
-                        break;
-                    }
-                    else if (_rightAdd > _n)
-                        break;
-                    else
-                        --_standardRight;
-                }
-                // Right
+//                    if (_rightAdd == _n)
+//                    {
+//                        ++_ret;
+//                        break;
+//                    }
+//                    else if (_rightAdd > _n)
+//                        break;
+//                    else
+//                        --_standardRight;
+//                }
+//                // Right
 
-                ++_left;
-                --_right;
-            }
+//                ++_left;
+//                --_right;
+//            }
 
-            Console.WriteLine(_ret);
-        }
-    }
-}
+//            Console.WriteLine(_ret);
+//        }
+//    }
+//}
