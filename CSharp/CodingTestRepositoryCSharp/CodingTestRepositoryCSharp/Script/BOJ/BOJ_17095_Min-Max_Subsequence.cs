6fcd67e632f41e1e36b54c12b2884b5fc82201a3
jonghow <sjh15348@gmail.com>
2024-12-22 22:42:53 +0900

    ProblemFail_BOJ_17095_Min-Max_Subsequence
    https://www.acmicpc.net/problem/17095

---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_17095_Min-Max_Subsequence.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using static CodingTestProj.Program;
+using System.Text;
+
+///*
+// * Difficulty : 
+// * URL : https://www.acmicpc.net/problem/17095
+//  * Time : 01h 02m + 
+// */
+
+namespace CodingTestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solu = new Solution();
+            solu.solve();
+        }
+    }
+
+    public class Solution
+    {
+        public int _n; 
+
+        public int _left; 
+        public int _right;
+
+        public int[] _arr;
+
+        public int _retVal; // 가장 큰 값
+        public int _retLength; //  출력 밸류
+
+        public void solve()
+        {
+            _n = int.Parse(Console.ReadLine());
+            _arr = new int[_n];
+
+            //string[] _input = new string[100001];
+            //for (int i = 0; i < _n; ++i)
+            //    _input[i] = "0";
+
+            string[] _input = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < _n; ++i)
+                _arr[i] = int.Parse(_input[i]);
+
+            _retVal = int.MinValue;
+            _retLength = int.MaxValue;
+
+            int _addRange;
+            _left = 0;
+            _right = _n - 1;
+
+            int _fixRight = _right;
+
+            while (_left < _right)
+            {
+                _addRange = 0;
+
+                while ((_left+_addRange) < _fixRight)
+                {
+                    int calcValue = Math.Abs(_arr[_left] - _arr[_left + _addRange]);
+                    int calcIndex = _addRange + 1;
+
+                    if (_retVal <= calcValue)
+                    {
+                        if (_retVal != calcValue)
+                        {
+                            _retLength = calcIndex;
+                        }
+                        else
+                        {
+                            _retLength = _retLength >= calcIndex ? calcIndex : _retLength;
+                        }
+                        _retVal = calcValue;
+                    }
+
+                    ++_addRange;
+                }
+                // 왼쪽 기준으로 서치
+
+                _addRange = 0;
+                while ((_right + _addRange) >= 0)
+                {
+                    int calcValue = Math.Abs(_arr[_right] - _arr[_right + _addRange]);
+                    int calcIndex = Math.Abs(_addRange) + 1;
+
+                    if (_retVal <= calcValue)
+                    {
+                        if (_retVal != calcValue)
+                        {
+                            _retLength = calcIndex;
+                        }
+                        else
+                        {
+                            _retLength = _retLength >= calcIndex ? calcIndex : _retLength;
+                        }
+
+                        _retVal = calcValue;
+                    }
+
+                    --_addRange;
+                }
+                // 오른쪽 기준으로 서치
+
+                ++_left;
+                --_right;
+            }
+
+            Console.WriteLine(_retLength);
+        }
+    }
+}
--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_4158_CD.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_4158_CD.cs
@@ -1,88 +1,88 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Xml.Linq;
-using static CodingTestProj.Program;
-using System.Text;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Xml.Linq;
+//using static CodingTestProj.Program;
+//using System.Text;
 
-///*
-// * Difficulty : Easy
-// * URL : https://www.acmicpc.net/problem/4158
-//  * Time : 22m 
-// */
+/////*
+//// * Difficulty : Easy
+//// * URL : https://www.acmicpc.net/problem/4158
+////  * Time : 22m 
+//// */
 
-namespace CodingTestProj
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Solution solu = new Solution();
-            solu.solve();
-        }
-    }
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Solution solu = new Solution();
+//            solu.solve();
+//        }
+//    }
 
-    public class Solution
-    {
-        public int _ret;
+//    public class Solution
+//    {
+//        public int _ret;
 
-        int _tc;
-        int _n; // 상근 CD 수
-        int _m; // 선영 CD 수
+//        int _tc;
+//        int _n; // 상근 CD 수
+//        int _m; // 선영 CD 수
 
-        // _n, _m <= 1000000
+//        // _n, _m <= 1000000
 
-        HashSet<int> _hs_N;
-        HashSet<int> _hs_M;
+//        HashSet<int> _hs_N;
+//        HashSet<int> _hs_M;
         
-        int _left;
-        int _right;
+//        int _left;
+//        int _right;
 
-        public void solve()
-        {
-            _hs_N = new HashSet<int>();
-            _hs_M = new HashSet<int>();
+//        public void solve()
+//        {
+//            _hs_N = new HashSet<int>();
+//            _hs_M = new HashSet<int>();
 
-            string _inputRead = string.Empty;
-            string _endFlag = string.Empty;
-
-
-            while (true)
-            {
-                _ret = 0;
-                _hs_N.Clear();
-                _hs_M.Clear();
-                _inputRead = string.Empty;
+//            string _inputRead = string.Empty;
+//            string _endFlag = string.Empty;
 
 
-                string _input = Console.ReadLine();
-                if (_input == "0 0") 
-                    break;
+//            while (true)
+//            {
+//                _ret = 0;
+//                _hs_N.Clear();
+//                _hs_M.Clear();
+//                _inputRead = string.Empty;
 
-                string[] _inputSeperate = _input.Split(' ');
 
-                _n = int.Parse(_inputSeperate[0]);
-                _m = int.Parse(_inputSeperate[1]);
+//                string _input = Console.ReadLine();
+//                if (_input == "0 0") 
+//                    break;
 
-                for (int i = 0; i < _n; ++i)
-                    _hs_N.Add(int.Parse(Console.ReadLine()));
+//                string[] _inputSeperate = _input.Split(' ');
 
-                for (int i = 0; i < _m; ++i)
-                    _hs_M.Add(int.Parse(Console.ReadLine()));
+//                _n = int.Parse(_inputSeperate[0]);
+//                _m = int.Parse(_inputSeperate[1]);
 
-                _left = 0;
-                _right = 0;
+//                for (int i = 0; i < _n; ++i)
+//                    _hs_N.Add(int.Parse(Console.ReadLine()));
 
-                foreach (var val in _hs_N)
-                {
-                    if (_hs_M.Contains(val))
-                    {
-                        ++_ret;
-                    }
-                }
+//                for (int i = 0; i < _m; ++i)
+//                    _hs_M.Add(int.Parse(Console.ReadLine()));
 
-                Console.WriteLine(_ret);
-            }
-        }
-    }
-}
+//                _left = 0;
+//                _right = 0;
+
+//                foreach (var val in _hs_N)
+//                {
+//                    if (_hs_M.Contains(val))
+//                    {
+//                        ++_ret;
+//                    }
+//                }
+
+//                Console.WriteLine(_ret);
+//            }
+//        }
+//    }
+//}
