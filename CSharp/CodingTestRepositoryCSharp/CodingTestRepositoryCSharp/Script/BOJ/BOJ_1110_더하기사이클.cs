7a1df0c0ba1fe524e3a7362a4fa14abc6464e73f
jonghow <sjh15348@gmail.com>
2024-12-31 16:06:20 +0900

    ProblemFail_BOJ_6230_BuyOneGetOneFree
    https://www.acmicpc.net/problem/6230

--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1110_더하기사이클.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_1110_더하기사이클.cs
@@ -1,73 +1,73 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Xml.Linq;
-using static CodingTestProj.Program;
-using System.Text;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Xml.Linq;
+//using static CodingTestProj.Program;
+//using System.Text;
 
-///*
-// * Difficulty : Easy
-// * URL : https://www.acmicpc.net/problem/1110
-//  * Time : 35m
-// */
+/////*
+//// * Difficulty : Easy
+//// * URL : https://www.acmicpc.net/problem/1110
+////  * Time : 35m
+//// */
 
-namespace CodingTestProj
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Solution solu = new Solution();
-            solu.solve();
-        }
-    }
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Solution solu = new Solution();
+//            solu.solve();
+//        }
+//    }
 
-    public class Solution
-    {
-        // 0 ~ 99 
-        public int _val;
-        public int _ret;
+//    public class Solution
+//    {
+//        // 0 ~ 99 
+//        public int _val;
+//        public int _ret;
 
-        // 10보다 작다 => 0을 붙인다.
+//        // 10보다 작다 => 0을 붙인다.
 
-        public void solve()
-        {
-            _val = int.Parse(Console.ReadLine());
-            _ret = 0;
+//        public void solve()
+//        {
+//            _val = int.Parse(Console.ReadLine());
+//            _ret = 0;
 
-            int _calc = _val;
+//            int _calc = _val;
 
-            while(true)
-            {
-                ++_ret;
+//            while(true)
+//            {
+//                ++_ret;
 
-                int _valRet = 0;
-                int _tempCalc = 0;
-                bool _isLower10 = (_calc < 10) ? true : false;
+//                int _valRet = 0;
+//                int _tempCalc = 0;
+//                bool _isLower10 = (_calc < 10) ? true : false;
 
-                if (_isLower10)
-                {
-                    _tempCalc = _calc * 10;
-                    _calc = _tempCalc;
-                }
+//                if (_isLower10)
+//                {
+//                    _tempCalc = _calc * 10;
+//                    _calc = _tempCalc;
+//                }
 
-                int _val1 = _calc / 10; // 십의자리
-                int _val2 = _calc % 10; // 일의자리
+//                int _val1 = _calc / 10; // 십의자리
+//                int _val2 = _calc % 10; // 일의자리
 
-                int _val3 = (_val1 + _val2) >= 10 ? (_val1 + _val2) % 10 : _val1 + _val2;
+//                int _val3 = (_val1 + _val2) >= 10 ? (_val1 + _val2) % 10 : _val1 + _val2;
 
-                if (_isLower10)
-                    _valRet = (_val1 * 10) + _val3;
-                else
-                    _valRet = (_val2 * 10) + _val3;
+//                if (_isLower10)
+//                    _valRet = (_val1 * 10) + _val3;
+//                else
+//                    _valRet = (_val2 * 10) + _val3;
 
-                if (_val == _valRet)
-                    break;
+//                if (_val == _valRet)
+//                    break;
 
-                _calc = _valRet;
-            }
+//                _calc = _valRet;
+//            }
 
-            Console.WriteLine(_ret);
-        }
-    }
-}
+//            Console.WriteLine(_ret);
+//        }
+//    }
+//}
---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_6230_F_BuyOneGetOneFree.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using static CodingTestProj.Program;
+using System.Text;
+
+///*
+// * Difficulty : 
+// * URL : https://www.acmicpc.net/problem/6230
+//  * Time : 1h +
+// */
+
+namespace CodingTestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solu = new Solution();
+            solu.solve();
+        }
+    }
+
+    public class Solution
+    {
+        public int _n;
+        public int _m;
+
+        public int[] _arrN;
+        public int[] _arrM;
+
+        public int _left;
+        public int _right;
+
+        public void solve()
+        {
+            string[] _input = Console.ReadLine().Split(' ');
+            _n = int.Parse(_input[0]);
+            _m = int.Parse(_input[1]);
+
+            _arrN = new int[_n];
+            _arrM = new int[_m];
+
+            for (int i = 0; i < _n; ++i)
+                _arrN[i] = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < _m; ++i)
+                _arrM[i] = int.Parse(Console.ReadLine());
+
+            /*
+             *  A크기 건초베일 하나 살때 B 크기의 건초베일 무료로
+             *  N 고품질 배일 , M 저품질 베일
+             */
+            _left = 0;
+            _right = _n - 1;
+
+            // 제일 큰 수에서 가져오면 된다.
+
+            //Array.Sort(_arrN, (int a, int b) => { return b.CompareTo(a); });
+            Array.Sort(_arrN);
+            Array.Sort(_arrM);
+
+            int _retResult = 0;
+
+            for(int i = 0; i < _arrN.Length; ++i)
+            {
+                int val = _arrN[i];
+                int retVal = BinarySearch(_arrM, val);
+                _retResult = Math.Max(_retResult, retVal+1);
+            }
+
+            Console.WriteLine(_retResult);
+        }
+
+        public int BinarySearch(int[] smallArr, int value)
+        {
+            int left = 0;
+            int right = smallArr.Length;
+
+            while(left < right)
+            {
+                int mid = (left + right) / 2;
+
+                if (smallArr[mid] < value)
+                    left = mid +1;
+                else
+                    right = mid;
+            }
+
+            return left;
+        }
+    }
+}
+ 
