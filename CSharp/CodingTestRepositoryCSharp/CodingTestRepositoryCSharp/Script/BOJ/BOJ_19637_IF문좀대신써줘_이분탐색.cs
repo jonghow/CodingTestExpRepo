e1c064a646f60126c6b16d41f77d27890e8101bb
jonghow <sjh15348@gmail.com>
2025-01-12 01:14:15 +0900

    ProblemSolve_BOJ_19637_IF문좀대신써줘
    https://www.acmicpc.net/problem/19637

---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_19637_IF문좀대신써줘_이분탐색.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using static CodingTestProj.Program;
+using System.Text;
+
+///*
+// * Difficulty : Middle
+// * URL : https://www.acmicpc.net/problem/19637
+//  * Time : 1h 26m
+// */
+
+namespace CodingTestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solu = new Solution();
+            solu.solve();
+        }
+    }
+
+    public class Solution
+    {
+        public int _titleCnt;
+        public int _chrCnt;
+        public List<KeyValuePair<int, string>> _lst_kvt;
+        public const int CTCnt = 10000001;
+
+        public int[] _arr;
+
+        public StringBuilder _sb; 
+
+        public void solve()
+        {
+            _sb = new StringBuilder();
+
+            _lst_kvt = new List<KeyValuePair<int, string>>();
+            _lst_kvt.Add(new KeyValuePair<int, string>(0, string.Empty));
+
+            string[] _input;
+            _input = Console.ReadLine().Split(' ');
+            _titleCnt = int.Parse(_input[0]);
+            _chrCnt = int.Parse(_input[1]);
+            _arr = new int[_chrCnt];
+
+            for (int i= 0; i < _titleCnt; i++){
+                _input = Console.ReadLine().Split(' ');
+                string _title = _input[0];
+                int _val = int.Parse(_input[1]);
+                _lst_kvt.Add(new KeyValuePair<int, string>(_val, _title));
+            }// title
+
+            
+            for(int i = 0; i < _chrCnt; ++i){
+                _arr[i] = int.Parse(Console.ReadLine());
+            }
+            //_chrCnt
+
+            for(int i = 0; i < _arr.Length; ++i)
+            {
+                _sb.AppendLine(BinarySearch(_arr[i]));
+            }
+
+            Console.WriteLine(_sb.ToString());
+        }
+
+        public string BinarySearch(int val){
+            string _ret = string.Empty;
+            int _left = 0;
+            int _right = _lst_kvt.Count - 1;
+
+            while(_left <= _right)
+            {
+                int mid = (_left + _right) / 2;
+
+                if (_left == mid || _right == mid)
+                {
+                    _ret = _lst_kvt[_right].Value;
+                    break;
+                }
+                else if (_lst_kvt[_left].Key <= val && _lst_kvt[mid].Key >= val)
+                {
+                    _right = mid;
+                }
+                else
+                    _left = mid;
+            }
+
+            return _ret;
+        }
+    }
+}
+
+
+
---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_19637_IF문좀대신써줘_풀배열.cs
@@ -0,0 +1,82 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Xml.Linq;
+//using static CodingTestProj.Program;
+//using System.Text;
+
+///*
+// * Difficulty : Middle
+// * URL : https://www.acmicpc.net/problem/19637
+//  * Time : 1h 26m
+// */
+
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Solution solu = new Solution();
+//            solu.solve();
+//        }
+//    }
+
+//    public class Solution
+//    {
+//        public int _titleCnt;
+//        public int _chrCnt;
+//        public Dictionary<int, string> _dct_title;
+//        public const int CTCnt = 10000001;
+
+//        public int[] _arr;
+
+//        public StringBuilder _sb; 
+
+//        public void solve()
+//        {
+//            _sb = new StringBuilder();
+
+//            _dct_title = new Dictionary<int, string>();
+//            for(int i = 0; i < CTCnt; i++)
+//                _dct_title.Add(i, string.Empty);
+
+//            string[] _input;
+//            _input = Console.ReadLine().Split(' ');
+//            _titleCnt = int.Parse(_input[0]);
+//            _chrCnt = int.Parse(_input[1]);
+//            _arr = new int[_chrCnt];
+
+//            int min = 0; 
+//            for (int i= 0; i < _titleCnt; i++){
+//                _input = Console.ReadLine().Split(' ');
+//                int _val = int.Parse(_input[1]);
+
+//                for(; min <= _val; ++min)
+//                {
+//                    _dct_title[min] =_input[0];
+//                }
+//            }// title
+
+
+//            for(int i = 0; i < _chrCnt; ++i){
+//                _arr[i] = int.Parse(Console.ReadLine());
+//            }
+//            //_chrCnt
+
+//            for(int i = 0; i < _arr.Length; ++i)
+//            {
+//                _sb.AppendLine(BinarySearch(_arr[i]));
+//            }
+
+//            Console.WriteLine(_sb.ToString());
+//        }
+
+//        public string BinarySearch(int val){
+//            return _dct_title[val];
+//        }
+//    }
+//}
+
+
+
