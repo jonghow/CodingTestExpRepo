2146e92a5a958bc5951fe1badc1f701ee4d7c5fd
jonghow <sjh15348@gmail.com>
2025-01-13 00:01:26 +0900

    ProblemFail_BOJ_11663_선분위의점
    https://www.acmicpc.net/problem/11663

---
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_11663_선분_위의_점.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using static CodingTestProj.Program;
+using System.Text;
+
+/*
+ * Difficulty : 
+ * URL : https://www.acmicpc.net/problem/11663
+ * Time : 
+ */
+
+public struct Point
+{
+    public int a;
+    public int b;
+
+    public Point(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+}
+
+namespace CodingTestProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solu = new Solution();
+            solu.solve();
+        }
+    }
+
+    public class Solution
+    {
+        public int _n;
+        public int _m;
+
+        public int[] _dot;
+        public Point[] _point;
+
+        public StringBuilder _sb;
+
+        public void solve(){
+            _sb = new StringBuilder();
+            string[] _input = Console.ReadLine().Split(' ');
+            _n = int.Parse(_input[0]);
+            _m = int.Parse(_input[1]);
+
+            _dot = new int[_n];
+            Array.Sort(_dot);
+
+            _input = Console.ReadLine().Split(' ');
+            for(int i = 0; i < _n; ++i)
+                _dot[i] = int.Parse(_input[i]);
+
+            _point = new Point[_m];
+
+            for(int i = 0; i < _m; ++i)
+            {
+                _input = Console.ReadLine().Split(' ');
+                _point[i].a = int.Parse(_input[0]);
+                _point[i].b = int.Parse(_input[1]);
+            }
+
+            for(int i = 0; i < _point.Length; ++i)
+            {
+                int _ret = BinarySearch(_dot, _point[i]);
+                _sb.AppendLine(_ret.ToString());
+            }
+ 
+            Console.WriteLine(_sb.ToString());
+        }
+
+        public int BinarySearch(int[] _arr, Point _pt)
+        {
+            int left = 0;
+            int right = _arr.Length - 1;
+            int _ret = 0;
+
+            while(left < right)
+            {
+                int mid = (left + right) / 2;
+
+                int calcleft = (left == 0) ? 0 : left - 1;
+
+                if (_arr[calcleft] <= _pt.a && _arr[right] >= _pt.b)
+                {
+                    _ret = right - left +1;
+                    break;
+                }
+                else if (_arr[left] < _pt.a)
+                {
+                    left = left + 1;
+                }
+                else
+                    right = mid + 1;
+            }
+
+            return _ret;
+        }
+    }
+}
+
+
+
--- CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_2775_암기왕.cs
+++ CSharp/CodingTestRepositoryCSharp/CodingTestRepositoryCSharp/Script/BOJ/BOJ_2775_암기왕.cs
@@ -1,71 +1,68 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Xml.Linq;
-using static CodingTestProj.Program;
-using System.Text;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Xml.Linq;
+//using static CodingTestProj.Program;
+//using System.Text;
 
-// * Difficulty : Easy
-// * URL : https://www.acmicpc.net/problem/2776
-//  * Time : 13m
-// */
+//// * Difficulty : Easy
+//// * URL : https://www.acmicpc.net/problem/2776
+////  * Time : 13m
+//// */
 
-namespace CodingTestProj
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Solution solu = new Solution();
-            solu.solve();
-        }
-    }
+//namespace CodingTestProj
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Solution solu = new Solution();
+//            solu.solve();
+//        }
+//    }
 
-    public class Solution
-    {
-        public int _tc;
-        public int _n;
-        public int _m;
+//    public class Solution
+//    {
+//        public int _tc;
+//        public int _n;
+//        public int _m;
 
-        public HashSet<int> _hs;
-        public void solve(){
+//        public HashSet<int> _hs;
+//        public void solve(){
 
-            // 수첩 1 => 하루동안 본 숫자
-            // 수첩 2 => 봤다고 주장
+//            // 수첩 1 => 하루동안 본 숫자
+//            // 수첩 2 => 봤다고 주장
 
-            StringBuilder sb = new StringBuilder();
-            _tc = int.Parse(Console.ReadLine());
-            _hs = new HashSet<int>();
+//            StringBuilder sb = new StringBuilder();
+//            _tc = int.Parse(Console.ReadLine());
+//            _hs = new HashSet<int>();
 
-            while (_tc > 0)
-            {
-                _hs.Clear();
-                //sb.Clear();
-                _n = int.Parse(Console.ReadLine());
-                string[] _input = Console.ReadLine().Split(' ');
+//            while (_tc > 0)
+//            {
+//                _hs.Clear();
+//                //sb.Clear();
+//                _n = int.Parse(Console.ReadLine());
+//                string[] _input = Console.ReadLine().Split(' ');
 
-                for(int i = 0; i < _input.Length; ++i)
-                    _hs.Add(int.Parse(_input[i]));
+//                for(int i = 0; i < _input.Length; ++i)
+//                    _hs.Add(int.Parse(_input[i]));
 
-                _m = int.Parse(Console.ReadLine());
-                _input = Console.ReadLine().Split(' ');
+//                _m = int.Parse(Console.ReadLine());
+//                _input = Console.ReadLine().Split(' ');
 
-                for (int i = 0; i < _input.Length; ++i)
-                {
-                    if (_hs.Contains(int.Parse(_input[i]))){
-                        sb.AppendLine("1");
-                    }
-                    else{
-                        sb.AppendLine("0");
-                    }
-                }
+//                for (int i = 0; i < _input.Length; ++i)
+//                {
+//                    if (_hs.Contains(int.Parse(_input[i]))){
+//                        sb.AppendLine("1");
+//                    }
+//                    else{
+//                        sb.AppendLine("0");
+//                    }
+//                }
 
-                --_tc;
-            }
-            Console.WriteLine(sb.ToString());
-        }
-    }
-}
-
-
-
+//                --_tc;
+//            }
+//            Console.WriteLine(sb.ToString());
+//        }
+//    }
+//}
